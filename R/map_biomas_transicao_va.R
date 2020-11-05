f8444244722e8702e589e8dd1a35d908675dad5f
BrunoAlcantaraD <66845709+BrunoAlcantaraD@users.noreply.github.com>
2020-11-04 21:02:13 -0300

    Tirei as maiúsculas

--- R/map_biomas_transicao_va.R
+++ R/map_biomas_transicao_va.R
@@ -1,16 +1,16 @@
-#' @title datazoom_transicao_mapbiomas
+#' @title datazoom_transicao_mapbiomas_va
 #' 
 #' @description Download and filter data on transition of types of soil covering by year
 #' 
-#' @param Path A string indicating where the raw data is in your computer. The default is NULL which means the data will be extracted directly from the website
-#' @param Code_State Output contains only data from the state selected
+#' @param path A string indicating where the raw data is in your computer. The default is NULL which means the data will be extracted directly from the website
+#' @param code_state Output contains only data from the state selected
 #' Input has to be IBGE's numeric coding for the state desired
-#' @param Covering_From Output contains only data of the transition from the covering selected to all other coverings
+#' @param covering_from Output contains only data of the transition from the covering selected to all other coverings
 #' Input has to be the code of the desired covering
-#' @param Covering_To Output contains only data of the transition from all coverings to the covering selected
+#' @param covering_to Output contains only data of the transition from all coverings to the covering selected
 #' Input has to be the code of the desired covering
-#' @param Type Decide if the output should have a column for each year (Normal) or a single column for the areas of all years (Stacked | Empilhado)
-#' @param Year_Diff A numeric object containing the year differential desired in the data base, or have all included (NULL)
+#' @param type Decide if the output should have a column for each year (normal) or a single column for the areas of all years (stacked | empilhado)
+#' @param year_diff A numeric object containing the year differential desired in the data base, or have all included (NULL)
 #' 
 #' @return A data base with data containing the area of each selected type of soil covering transition in each selected year
 #'
@@ -24,21 +24,21 @@
 #' @export
 #'
 #' @examples
-#' datazoom_transicao_mapbiomas(Path = '/Desktop',
-#'                              Code_State = 11, Covering_From = 3, Covering_To = 19,
-#'                              Type = 'Normal', Year_Diff = 5)
+#' datazoom_transicao_mapbiomas_va(path = '/Desktop',
+#'                              code_state = 11, covering_from = 3, covering_to = 19,
+#'                              type = 'normal', year_diff = 5)
 
 
 
-datazoom_transicao_mapbiomas_va<-function(Path = NULL,
-                                       Code_State = NULL, Covering_From = NULL, Covering_To = NULL,
-                                       Type = c('Stacked','Normal','Empilhado'), Year_Diff = NULL){
-  if(is.null(Path)){
+datazoom_transicao_mapbiomas_va<-function(path = NULL,
+                                       code_state = NULL, covering_from = NULL, covering_to = NULL,
+                                       type = c('stacked','normal','empilhado'), year_diff = NULL){
+  if(is.null(path)){
     url1<-'https://drive.google.com/uc?export=download&id=1SF2BdX-UMTCFOIaAi9uKuf11Yg8Os_vs'
     p1f <- tempfile()
     drive_download(url1, p1f)
   }else{
-    p1f<-paste0(Path,'/Dados_Transicao_MapBiomas 4.1_BIOMAS-UF-MUN_SITE.xlsx')
+    p1f<-paste0(path,'/Dados_Transicao_MapBiomas 4.1_BIOMAS-UF-MUN_SITE.xlsx')
   }
   a<-read_excel(path = p1f, sheet = 2)
   a<-a[!(a$bioma!="AMAZONIA"),]
@@ -46,17 +46,17 @@ datazoom_transicao_mapbiomas_va<-function(Path = NULL,
   retorno<-data.frame()
   tab<-a
   ncolu<-ncol(a)
-  if(!is.null(Code_State)){
-    tab<-tab[!(tab$codigobiomasestados!=Code_State),]
+  if(!is.null(code_state)){
+    tab<-tab[!(tab$codigobiomasestados!=code_state),]
   }
-  if(!is.null(Covering_From)){
-    tab<-tab[!(tab$de!=Covering_From),]
+  if(!is.null(covering_from)){
+    tab<-tab[!(tab$de!=covering_from),]
   }
-  if(!is.null(Covering_To)){
-    tab<-tab[!(tab$para!=Covering_To),]
+  if(!is.null(covering_to)){
+    tab<-tab[!(tab$para!=covering_to),]
   }
-  if(is.null(Year_Diff)){
-    if(Type=='Stacked' | Type=='Empilhado'){
+  if(is.null(year_diff)){
+    if(type=='stacked' | type=='empilhado'){
       for(i in 12:ncolu){
         ret<-c()
         ret<-tab[,1:11]
@@ -75,7 +75,7 @@ datazoom_transicao_mapbiomas_va<-function(Path = NULL,
         }
       }
     }
-    if(Type=='Normal'){
+    if(type=='normal'){
       ret<-c()
       ret<-tab[,1:11]
       for(i in 12:ncolu){
@@ -90,8 +90,8 @@ datazoom_transicao_mapbiomas_va<-function(Path = NULL,
       }
       retorno<-ret
     }
-  }else if(!is.null(Year_Diff)){
-    if(Type=='Stacked' | Type=='Empilhado'){
+  }else if(!is.null(year_diff)){
+    if(type=='stacked' | type=='empilhado'){
       for(i in 12:ncolu){
         ret<-c()
         ret<-tab[,1:11]
@@ -110,7 +110,7 @@ datazoom_transicao_mapbiomas_va<-function(Path = NULL,
         }
       }
     }
-    if(Type=='Normal'){
+    if(type=='normal'){
       ret<-c()
       ret<-tab[,1:11]
       for(i in 12:ncolu){
@@ -119,7 +119,7 @@ datazoom_transicao_mapbiomas_va<-function(Path = NULL,
         anof<-substring(nomecol,8,12)
         anoi<-as.integer(anoi)
         anof<-as.integer(anof)
-        if(anof-anoi==Year_Diff){
+        if(anof-anoi==year_diff){
           ret<-cbind(ret,tab[,i])
         }
       }
