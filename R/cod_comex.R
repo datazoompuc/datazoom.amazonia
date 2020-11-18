93339233fb316437fa5cca3d38842160d2db4f98
annasaraiva <annasaraiva@users.noreply.github.com>
2020-11-18 20:04:11 -0300

    Fogo no parquinho
    O tempo que essa função tava demorando para rodar estava desafiando a minha sanidade, então decidi mudar um pouco a estrutura para ver se ia mais rápido E FUNCIONOU

--- R/cod_comex.R
+++ R/cod_comex.R
@@ -20,22 +20,28 @@
 #'
 #' @export
 #'
-#' @examples comex_data(c(2000,2001,2002), ncm = TRUE, exp = TRUE)
-#' comex_data(c(1997,2004,2008), sh4 = TRUE, imp = TRUE, language = "eng")
+#' @examples load_comex(c(2000,2001,2002), ncm = TRUE, exp = TRUE)
+#' load_comex(c(1997,2004,2008), sh4 = TRUE, imp = TRUE, language = "eng")
 #'
 
-comex_data <- function(year, ncm = FALSE, sh4 = FALSE, exp = FALSE, imp = FALSE, language = "pt") {
-#Unless english is specified, columns will be renamed using Portuguese labels
-    if (language != "pt" && language != "eng") {
+load_comex <- function(year, ncm = FALSE, sh4 = FALSE, exp = FALSE, imp = FALSE, language = "pt") {
+  warning("Depending on amount of items selected function may take time to run")
+  #Unless english is specified, columns will be renamed using Portuguese labels
+  if (language != "pt" && language != "eng") {
     warning("Language selected not supported! Proceding with Portuguese")
-    }
+  }
   #removes invalid years from input vector
   year1 <- year[year <= 2020]
-  year1 <- year1[ year1 >= 1997]
+  year1 <- year1[year1 >= 1997]
   year2 <- year[year >= 1989]
   year2 <- year2[year2 <= 1996]
   #creates list to store data frames
-  all_dtframes <- list()
+  data_1 <- data.frame()
+  data_2 <- data.frame()
+  data_3 <- data.frame()
+  data_4 <- data.frame()
+  data_5 <- data.frame()
+  data_6 <- data.frame()
   #first generates export data frames
   if (exp == TRUE) {
     #after 1997 two types of codes for goods are supported
@@ -44,171 +50,163 @@ comex_data <- function(year, ncm = FALSE, sh4 = FALSE, exp = FALSE, imp = FALSE,
       if (ncm == TRUE) {
         #Generates URLs for each year to download .csv file from website
         url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/EXP_", year1, ".csv")
-        #creates data frame to store
-        data_exp_ncm <- data.frame()
         #for each year, concatenates data frame to all previous year
         for (i in 1:length(url)) {
-          df <- read.csv2(url[i])
-          data_exp_ncm <- rbind(data_exp_ncm, df)
+          df <- readr::read_csv2(url[i])
+          data_1 <- rbind(data_1, df)
         }
         #relabels columns in selected language
         if (language == "eng") {
-          colnames(data_exp_ncm) <- c(
+          colnames(data_1) <- c(
             "Year", "Month", "Code Mercosul Common Nomenclature", "Statistical Unit Code", "Country code",
             "Acronym of the exporting unit of the federation", "Code of the mode of transport", "Code for the customs processing unit",
             "Statistical Quantity", "Quantity in net kg", "Free on Board value"
           )
         } else {
-          colnames(data_exp_ncm) <- c(
+          colnames(data_1) <- c(
             "Ano", "Mês", "Código Nomentaclatura Comum do Mercosul", "Código da Unidade Estatística", "Código do país",
             "Sigla da Unidade da Federação exportadora", "Código da via de Transporte", "Código da Unidade da Receita Federal",
             "Quantidade Estatística", "Quantidade em Kg Líquidos", "Valor Free on Board"
           )
         }
-        #stores generated data frame in list
-        all_dtframes <- list(all_dtframes, data_exp_ncm)
       }
       #same structure used above but for another system
       if (sh4 == TRUE) {
         url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/mun/EXP_", year1, "_MUN.csv")
-        data_exp_sh4 <- data.frame()
+
         for (i in 1:length(url)) {
-          df <- read.csv2(url[i])
-          data_exp_sh4 <- rbind(data_exp_sh4, df)
+          df <- readr::read_csv2(url[i])
+          data_2 <- rbind(data_2, df)
         }
         if (language == "eng") {
-          colnames(data_exp_sh4) <- c(
+          colnames(data_2) <- c(
             "Year", "Month", "Code Harmonized System", "Country code",
             "Acronym of the exporting unit of the federation", "Code of Fiscal Domicile Municipality",
             "Quantity in net kg", "Free on Board value"
           )
         } else {
-          colnames(data_exp_sh4) <- c(
+          colnames(data_2) <- c(
             "Ano", "Mês", "Código Sistema Harmonizado", "Código do país",
             "Sigla da Unidade da Federação exportadora", "Código Município do Domicílio Fiscal",
             "Quantidade em Kg Líquidos", "Valor Free on Board"
           )
         }
-        all_dtframes <- list(all_dtframes, data_exp_sh4)
       } else if (sh4 == FALSE & ncm == FALSE) {
         #warning to help user in case neither systems are set as TRUE in input
         warning("Type of data (Nomenclatura Comum do Mercosul or Sistema Harmonizado) not selected")
       }
       #before 1997 only one other system is supported, but structure is the same
-    } else if (length(year2) != 0) {
+    }
+    if (length(year2) != 0) {
       url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/nbm/EXP_", year2, "_NBM.csv")
-      data_exp_nbm <- data.frame()
+
       for (i in 1:length(url)) {
-        df <- read.csv2(url[i])
-        data_exp_nbm <- rbind(data_exp_nbm, df)
+        df <- readr::read_csv2(url[i])
+        data_3 <- rbind(data_3, df)
       }
 
       if (language == "eng") {
-        colnames(data_exp_nbm) <- c(
+        colnames(data_3) <- c(
           "Year", "Month", "Code Brazilian Goods Nomenclature", "Country code",
           "Acronym of the exporting unit of the federation",
           "Quantity in net kg", "Free on Board value"
         )
       } else {
-        colnames(data_exp_nbm) <- c(
+        colnames(data_3) <- c(
           "Ano", "Mês", "Código Nomentaclatura Brasileira de Mercadorias", "Código do país",
           "Sigla da Unidade da Federação exportadora",
           "Quantidade em Kg Líquidos", "Valor Free on Board"
         )
       }
-
-      all_dtframes <- list(all_dtframes, data_exp_nbm)
-    } else {
+    } else if (length(year1)==0 && length(year2)==0) {
       warning("Year selected not valid")
     }
-  } else if (imp == TRUE) {
+  }
+  if (imp == TRUE) {
     #same structure as exports, but for the import data
     #separate sets of data frame are generated
     if (length(year1) != 0) {
       if (ncm == TRUE) {
         url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/IMP_", year1, ".csv")
-        data_imp_ncm <- data.frame()
+
         for (i in 1:length(url)) {
-          df <- read.csv2(url[i])
-          data_imp_ncm <- rbind(data_imp_ncm, df)
+          df <- readr::read_csv2(url[i])
+          data_4 <- rbind(data_4, df)
         }
 
         if (language == "eng") {
-          colnames(data_imp_ncm) <- c(
+          colnames(data_4) <- c(
             "Year", "Month", "Code Mercosul Common Nomenclature", "Statistical Unit Code", "Country code",
             "Acronym of the importing unit of the federation", "Code of the mode of transport", "Code for the customs processing unit",
             "Statistical Quantity", "Quantity in net kg", "Free on Board value"
           )
         } else {
-          colnames(data_imp_ncm) <- c(
+          colnames(data_4) <- c(
             "Ano", "Mês", "Código Nomentaclatura Comum do Mercosul", "Código da Unidade Estatística", "Código do país",
             "Sigla da Unidade da Federação importadora", "Código da via de Transporte", "Código da Unidade da Receita Federal",
             "Quantidade Estatística", "Quantidade em Kg Líquidos", "Valor Free on Board"
           )
         }
 
-        all_dtframes <- list(all_dtframes, data_imp_ncm)
       }
       if (sh4 == TRUE) {
         url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/mun/IMP_", year1, "_MUN.csv")
-        data_imp_sh4 <- data.frame()
+
         for (i in 1:length(url)) {
-          df <- read.csv2(url[i])
-          data_imp_sh4 <- rbind(data_imp_sh4, df)
+          df <- readr::read_csv2(url[i])
+          data_5 <- rbind(data_5, df)
         }
 
         if (language == "eng") {
-          colnames(data_imp_sh4) <- c(
+          colnames(data_5) <- c(
             "Year", "Month", "Code Harmonized System", "Country code",
             "Acronym of the importing unit of the federation", "Code of Fiscal Domicile Municipality",
             "Quantity in net kg", "Free on Board value"
           )
         } else {
-          colnames(data_imp_sh4) <- c(
+          colnames(data_5) <- c(
             "Ano", "Mês", "Código Sistema Harmonizado", "Código do país",
             "Sigla da Unidade da Federação importadora", "Código Município do Domicílio Fiscal",
             "Quantidade em Kg Líquidos", "Valor Free on Board"
           )
         }
 
-        all_dtframes <- list(all_dtframes, data_imp_sh4)
       }
-    } else if (length(year2) != 0) {
+    }
+    if (length(year2) != 0) {
       url <- paste0("http://www.balanca.economia.gov.br/balanca/bd/comexstat-bd/nbm/IMP_", year2, "_NBM.csv")
-      data_imp_nbm <- data.frame()
+
       for (i in 1:length(url)) {
-        df <- read.csv2(url[i])
-        data_imp_nbm <- rbind(data_imp_nbm, df)
+        df <- readr::read_csv2(url[i])
+        data_6 <- rbind(data_6, df)
       }
 
       if (language == "eng") {
-        colnames(data_imp_nbm) <- c(
+        colnames(data_6) <- c(
           "Year", "Month", "Code Brazilian Goods Nomenclature", "Country code",
           "Acronym of the importing unit of the federation",
           "Quantity in net kg", "Free on Board value"
         )
       } else {
-        colnames(data_imp_nbm) <- c(
+        colnames(data_6) <- c(
           "Ano", "Mês", "Código Nomentaclatura Brasileira de Mercadorias", "Código do país",
           "Sigla da Unidade da Federação importadora",
           "Quantidade em Kg Líquidos", "Valor Free on Board"
         )
       }
 
-      all_dtframes <- list(all_dtframes, data_imp_nbm)
     }
-  } else {
+  }
+  if(exp == FALSE && imp == FALSE) {
     warning("Export or import not selected.")
     #just like the systems, warning is generated it neither import or export are set to TRUE in input
   }
-  #removes first entry of list, which is NA
-  all_dtframes[1] <- NULL
+
+  all_dtframes <- list(data_1,data_2,data_3,data_4,data_5,data_6)
+  all_dtframes = all_dtframes[lapply(all_dtframes,length)>0]
   #returns list of data frames, one for each combination of export/import and system
   return(all_dtframes)
 }
-
-# all_dtframes <- list(data_exp_ncm, data_exp_sh4, data_exp_nbm, data_imp_ncm, data_imp_sh4, data_imp_nbm)
-# all_dtframes <- all_dtframes[-which(sapply(all_dtframes, is.na.data.frame))]
 
 # CO_ANO = "Ano"
 # Ano = "Year"
