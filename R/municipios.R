5427d9aec4ba4af1e01d169b3a4c4d7cfcc05192
Victor Aliende da Matta <victormatta14@gmail.com>
2020-12-03 13:42:23 -0300

    requested changes

--- R/municipios.R
+++ R/municipios.R
@@ -3,7 +3,7 @@ NULL
 
 #' Gets GDP at current prices for the Brazilian Legal Amazon
 #'
-#' @param years A numeric vector with years of interest.
+#' @param years A numeric vector with years of interest. Supported years are 2002-2017.
 #' @param aggregation_level A string that indicates the level of aggregation of the data. It can be by "City" or
 #'   "State"
 #' @param language A string that indicates in which language the data will be returned. The default is "eng", so your data will be returned in English.
@@ -19,7 +19,7 @@ load_pib <- function(years, aggregation_level = "City", language = "eng") {
 
   df <- tibble::as_tibble(
     sidrar::get_sidra(
-      5938,
+      5938, # Table code at Sidra
       period = as.character(years),
       variable = 37,
       geo = rep(aggregation_level, times = nrow(states)),
@@ -32,20 +32,26 @@ load_pib <- function(years, aggregation_level = "City", language = "eng") {
   if (aggregation_level == "City") {
     df <- df[df[["Munic\u00edpio (C\u00f3digo)"]] %in% legal_amazon$code, ] %>%
       dplyr::select("Munic\u00edpio (C\u00f3digo)", "Munic\u00edpio", "Ano", "Valor") %>%
-      dplyr::rename(PIB = Valor)
+      dplyr::rename(PIB = .data$Valor)
 
     if (language == "eng") {
       colnames(df) <- c("City (code)", "City", "Year", "GDP")
+    }
+    else if (language != "pt") {
+      warning("Selected language is not supported. Proceeding with Portuguese.")
     }
   }
   else {
     df <- df %>%
       dplyr::select("Unidade da Federa\u00e7\u00e3o (C\u00f3digo)", "Unidade da Federa\u00e7\u00e3o", "Ano", "Valor") %>%
-      dplyr::rename(PIB = Valor)
+      dplyr::rename(PIB = .data$Valor)
 
 
     if (language == "eng") {
       colnames(df) <- c("State (code)", "State", "Year", "Variable", "GDP")
+    }
+    else if (language != "pt") {
+      warning("Selected language is not supported. Proceeding with Portuguese.")
     }
   }
 
