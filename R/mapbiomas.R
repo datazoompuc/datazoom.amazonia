2fbafd90400370441ab2fb0a3e62d7599a98ded7
BrunoAlcantaraD <66845709+BrunoAlcantaraD@users.noreply.github.com>
2021-01-22 19:04:48 -0300

    Update mapbiomas.R

--- R/mapbiomas.R
+++ R/mapbiomas.R
@@ -8,7 +8,6 @@
 #' Input has to be the code of the desired covering
 #' ATT.: This code ranges from 1 to 5 and is different from the covering code for the other functions in this package
 #' Ex.: 1-Forest; 2-Non Forest Natural Formation; 3-Farming; 4-Non Vegetated Area; 5-Water
-#' @param type Decide if the output should have a column for each year (normal) or a single column for the areas of all years (stacked | empilhado)
 #' @param years A vector of numbers containing the years desired in the data base (years available are 1985 to 2020)
 #' Ex.: years = c(1985:2020)
 #' 
@@ -103,7 +102,6 @@ load_mapbiomas_covering<-function(aggregation_level = c('municipality', 'state',
 #' @description Download and filter data on transition of types of soil covering by year
 #' 
 #' @param path A string indicating where the raw data is in your computer. The default is NULL which means the data will be extracted directly from the website
-#' @param type Decide if the output should have a column for each year (normal) or a single column for the areas of all years (stacked | empilhado)
 #' @param transition_interval A numeric object containing the desired interval in years to observe transitions desired in the data base (1, 2, 5 or 10), or have all included (NULL)
 #' 
 #' @return A data base with data containing the area of each selected type of soil covering transition in each selected year
