eda8ea2a2c00a4131c567888ce2ee641805ddb39
Daniel AC Barbosa <daniel.adrianocb@gmail.com>
2021-02-02 15:27:04 -0300

    Supressing Message to User

--- R/nightlights.R
+++ R/nightlights.R
@@ -8,12 +8,14 @@
 #' @param var Variable belonging to the db dataset
 #' @param year_begin Year Begin
 #' @param year_end Year End
-#' @param type Geometry Type
+#' @param type Geometry Type. Can be either "municip" or "uf". (TO-DO: Next version will include support for rasters)
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#'
+#  #ggplot2::ggplot() + ggplot2::geom_sf(data = sf::st_as_sf(n[n$year==2013,]),ggplot2::aes(fill = mean_rad))
 #'
 load_ee = function(db = 'NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG',var='avg_rad',year_begin = '2013',year_end = '2018',
                    type='munic',nclusters=0){
@@ -49,24 +51,21 @@ load_ee = function(db = 'NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG',var='avg_rad',year_
     nlight$year = ceiling(nlight$month/12)+as.numeric(year_begin)-1
 
     ## This will depend on panel type
-
     nlight_avg = nlight %>% dplyr::group_by(id,year) %>%
-      dplyr::summarise(mean_rad = mean(avg_rad),sd_avg_rad = sd(avg_rad),geom=sf::st_union(geometry)) %>%
+      dplyr::summarise(mean_rad = mean(avg_rad),sd_avg_rad = sd(avg_rad),
+                       geom=sf::st_union(geometry)) %>%
       dplyr::ungroup()
 
     return(nlight_avg)
 
   }
 
+
   units = unique(shp$abbrev_state)
   # units = units[which(units != "SP")]
 
-  nlight = purrr::map_df(units,ext_fun)
-
-  # ggplot2::ggplot() + ggplot2::geom_sf(data = sf::st_as_sf(nlight_avg[nlight_avg$year==2013,]),
-  #                                      ggplot2::aes(fill = mean_rad))
-
-  # df = left_join(geo,nlight_avg)
+  if (nclusters == 0){nlight = suppressMessages(purrr::map_df(units,ext_fun))}
+  #if (nclusters > 0){nlight = pbapply::pblapply(units,ext_fun)} # We also have to unlist
 
   return(nlight)
 
