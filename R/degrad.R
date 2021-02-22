64a2e311b27e3442e3ecdb399d54c9e023cd5e5d
Victor Aliende da Matta <victormatta14@gmail.com>
2021-02-22 14:55:08 -0300

    improve documentation

--- R/degrad.R
+++ R/degrad.R
@@ -58,7 +58,7 @@ load_degrad <- function(source, space_aggregation = "municipality", time_aggrega
 #'
 #' @param source A number of different sources are supported:
 #'
-#' Passing a numeric \code{vector} of years will download the corresponding data from the INPE website. Available years are 2007-2016.
+#' Passing a numeric \code{vector} of years will download the corresponding DEGRAD datasets from INPE's website. Available versions are 2007-2016.
 #'
 #' Passing a \code{string} with a directory's path will read data from all shapefiles in the directory.
 #'
