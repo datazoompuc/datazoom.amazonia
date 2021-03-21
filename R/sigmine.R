446b2da318bd1a487a6b33c26c31a962db0d86e3
BrunoAlcantaraD <BrunoAlcantaraD@users.noreply.github.com>
2021-03-20 22:08:13 -0300

    Add municip code

--- R/sigmine.R
+++ R/sigmine.R
@@ -1,14 +1,14 @@
 #' @title load_sigmine
-#' 
+#'
 #' @description Download and filter data on the mines being explored legally in Brazil
-#' 
+#'
 #' @param space_aggregation A string that indicates the level of aggregation of the data. It can be by municipality or state
 #' @param source A string indicating where the raw data is in your computer. The default is NULL which means the data will be extracted directly from the website
-#' 
+#'
 #' @return A data base with data containing the area of each selected type of soil covering in each selected year
 #'
 #' @importFrom utils unzip
-#' 
+#'
 #' @author DataZoom, Department of Economics, Pontifical Catholic University of Rio de Janeiro
 #'
 #' @encoding UTF-8
@@ -33,7 +33,7 @@ load_sigmine<-function(space_aggregation = c('municipality', 'state','municipio'
   dir <- gsub(p1f, pattern = "\\.zip", replacement = "")
   unzip(p1f, exdir = dir)
   a<-sf::read_sf(dir)
-  
+
   if(space_aggregation == 'state' | space_aggregation == 'estado'){
     names(a)[names(a) == 'NUMERO'] <- 'numero'
     names(a)[names(a) == 'ULT_EVENTO'] <- 'ultimo_evento'
@@ -47,13 +47,13 @@ load_sigmine<-function(space_aggregation = c('municipality', 'state','municipio'
     names(a)[names(a) == 'USO'] <- 'uso'
     names(a)[names(a) == 'AREA_HA'] <- 'area_hectares'
     ret <- a
-    
+
   }else if(space_aggregation == 'municipality' | space_aggregation == 'municipio'){
     geo_amazon <- download_map()
     operation_crs <- sf::st_crs("+proj=poly +lat_0=0 +lon_0=-54 +x_0=5000000 +y_0=10000000 +ellps=aust_SA +units=m +no_defs")
     a <- sf::st_transform(a, operation_crs)
     geo_amazon <- sf::st_transform(geo_amazon, operation_crs)
-    
+
     sf::st_agr(a) = "constant"
     sf::st_agr(geo_amazon) = "constant"
     al<-sf::st_zm(a)
@@ -61,12 +61,11 @@ load_sigmine<-function(space_aggregation = c('municipality', 'state','municipio'
       dplyr::mutate(calculated_area = sf::st_area(.data$geometry)) %>%
       dplyr::group_by(.data$abbrev_state, .data$ANO) %>%
       sf::st_drop_geometry()
-    
+
     al$AREA_HA <- NULL
     al$UF <- NULL
     al$code_region <- NULL
     al$code_state <- NULL
-    al$code_muni <- NULL
     al$ULT_EVENTO <- NULL
     al$NUMERO <- NULL
     al$name_region <- NULL
