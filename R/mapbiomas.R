1444b47474f8f6be7add1d76b287cae7747c10e2
BrunoAlcantaraD <66845709+BrunoAlcantaraD@users.noreply.github.com>
2021-03-30 15:35:32 -0300

    Update mapbiomas.R

--- R/mapbiomas.R
+++ R/mapbiomas.R
@@ -28,7 +28,7 @@
 
 
 load_mapbiomas_covering<-function(space_aggregation = c('municipality', 'state','municipio','estado'), path = NULL,
-                                       covering = NULL, years = c(1985:2020)){
+                                  covering = NULL, years = c(1985:2020)){
   if(is.null(path)){
     if(space_aggregation=='state' | space_aggregation=='estado'){
       url1<-'https://mapbiomas-br-site.s3.amazonaws.com/Estat%C3%ADsticas/Dados_Cobertura_MapBiomas_5.0_UF-BIOMAS_SITE.xlsx'
@@ -61,17 +61,17 @@ load_mapbiomas_covering<-function(space_aggregation = c('municipality', 'state',
     if(!is.null(covering)){
       tab<-tab[!(substring(tab$level_1,1,1)!=covering),]
     }
-      for(i in years){
-        ret<-c()
-        ret<-tab[,1:8]
-        ano<-as.character(i)
-        ret<-cbind(ret,tab[,ano])
-        colnames(ret)[which(colnames(ret) == ano)] <- 'Area'
-        num<-nrow(tab[,ano])
-        data<-rep(c(i),num)
-        ret<-cbind(ret,data)
-        retorno<-rbind(retorno,ret)
-      }
+    for(i in years){
+      ret<-c()
+      ret<-tab[,1:8]
+      ano<-as.character(i)
+      ret<-cbind(ret,tab[,ano])
+      colnames(ret)[which(colnames(ret) == ano)] <- 'Area'
+      num<-nrow(tab[,ano])
+      data<-rep(c(i),num)
+      ret<-cbind(ret,data)
+      retorno<-rbind(retorno,ret)
+    }
   }else if(space_aggregation=='municipality' | space_aggregation=='municipio'){
     b<-read_excel(path = p1f, sheet = 3)
     tipos<-c('RR','RO','AC','AM','MA','TO','PA','AP','MT')
@@ -82,17 +82,17 @@ load_mapbiomas_covering<-function(space_aggregation = c('municipality', 'state',
     if(!is.null(covering)){
       tab<-tab[!(substring(tab$level_1,1,1)!=covering),]
     }
-      for(i in years){
-        ret<-c()
-        ret<-tab[,1:8]
-        ano<-as.character(i)
-        ret<-cbind(ret,tab[,ano])
-        colnames(ret)[which(colnames(ret) == ano)] <- 'Area'
-        num<-nrow(tab[,ano])
-        data<-rep(c(i),num)
-        ret<-cbind(ret,data)
-        retorno<-rbind(retorno,ret)
-      }
+    for(i in years){
+      ret<-c()
+      ret<-tab[,1:8]
+      ano<-as.character(i)
+      ret<-cbind(ret,tab[,ano])
+      colnames(ret)[which(colnames(ret) == ano)] <- 'Area'
+      num<-nrow(tab[,ano])
+      data<-rep(c(i),num)
+      ret<-cbind(ret,data)
+      retorno<-rbind(retorno,ret)
+    }
   }
   return(retorno)
 }
@@ -101,6 +101,7 @@ load_mapbiomas_covering<-function(space_aggregation = c('municipality', 'state',
 #' 
 #' @description Download and filter data on transition of types of soil covering by year
 #' 
+#' @param space_aggregation A string that indicates the level of aggregation of the data. It can be by municipality or state
 #' @param path A string indicating where the raw data is in your computer. The default is NULL which means the data will be extracted directly from the website
 #' @param transition_interval A numeric object containing the desired interval in years to observe transitions desired in the data base (1, 2, 5 or 10), or have all included (NULL)
 #' 
@@ -115,34 +116,34 @@ load_mapbiomas_covering<-function(space_aggregation = c('municipality', 'state',
 #' @export
 #'
 #' @examples
-#' load_mapbiomas_transition(path = NULL, transition_interval = 5)
+#' load_mapbiomas_transition(space_aggregation = 'municipality', path = NULL, transition_interval = 5)
 
 
 
 load_mapbiomas_transition<-function(space_aggregation = c('municipality', 'state','municipio','estado'), path = NULL, transition_interval = NULL){
   if(space_aggregation=='state' | space_aggregation=='estado'){
     if(is.null(path)){
-    url1<-'https://mapbiomas-br-site.s3.amazonaws.com/Estat%C3%ADsticas/Dados_Transicao_MapBiomas_5.0_UF-BIOMAS_SITE.xlsx'
-    p1f <- tempfile()
-    download.file(url1, p1f, mode="wb")
-  }else{
-    p1f<-paste0(path,'/Dados_Transicao_MapBiomas_5.0_UF-BIOMAS_SITE.xlsx')
-  }
-  a<-read_excel(path = p1f, sheet = 3)
-  a<-a[!(substring(a$biome,1,4)!="Amaz"),]
-  a$state[a$state == "Acre"] <- "AC"
-  a$state[substring(a$state,1,4) == "Amap"] <- "AP"
-  a$state[a$state == "Amazonas"] <- "AM"
-  a$state[a$state == "Tocantins"] <- "TO"
-  a$state[a$state == "Mato Grosso"] <- "MT"
-  a$state[substring(a$state,1,6) == "Maranh"] <- "MA"
-  a$state[substring(a$state,1,4) == "Rond"] <- "RO"
-  a$state[a$state == "Roraima"] <- "RR"
-  a$state[substring(a$state,1,3) == "Par"] <- "PA"
-  retorno<-data.frame()
-  tab<-a
-  ncolu<-ncol(a)
-  if(is.null(transition_interval)){
+      url1<-'https://mapbiomas-br-site.s3.amazonaws.com/Estat%C3%ADsticas/Dados_Transicao_MapBiomas_5.0_UF-BIOMAS_SITE.xlsx'
+      p1f <- tempfile()
+      download.file(url1, p1f, mode="wb")
+    }else{
+      p1f<-paste0(path,'/Dados_Transicao_MapBiomas_5.0_UF-BIOMAS_SITE.xlsx')
+    }
+    a<-read_excel(path = p1f, sheet = 3)
+    a<-a[!(substring(a$biome,1,4)!="Amaz"),]
+    a$state[a$state == "Acre"] <- "AC"
+    a$state[substring(a$state,1,4) == "Amap"] <- "AP"
+    a$state[a$state == "Amazonas"] <- "AM"
+    a$state[a$state == "Tocantins"] <- "TO"
+    a$state[a$state == "Mato Grosso"] <- "MT"
+    a$state[substring(a$state,1,6) == "Maranh"] <- "MA"
+    a$state[substring(a$state,1,4) == "Rond"] <- "RO"
+    a$state[a$state == "Roraima"] <- "RR"
+    a$state[substring(a$state,1,3) == "Par"] <- "PA"
+    retorno<-data.frame()
+    tab<-a
+    ncolu<-ncol(a)
+    if(is.null(transition_interval)){
       for(i in 15:ncolu){
         ret<-c()
         ret<-tab[,1:14]
@@ -160,7 +161,7 @@ load_mapbiomas_transition<-function(space_aggregation = c('municipality', 'state
           retorno<-rbind(retorno,ret)
         }
       }
-  }else if(!is.null(transition_interval)){
+    }else if(!is.null(transition_interval)){
       for(i in 15:ncolu){
         ret<-c()
         ret<-tab[,1:14]
@@ -185,23 +186,23 @@ load_mapbiomas_transition<-function(space_aggregation = c('municipality', 'state
     download.file(url1, p1f, mode="wb")
     a<-read_excel(path = p1f, sheet = 3)
     retorno<-data.frame()
-  tab<-a
-  ncolu<-ncol(a)
-  ret<-c()
-  ret<-tab[,1:15]
-  if(!is.null(transition_interval)){
-    for(i in 16:ncolu){
-      nomecol<-colnames(a)[i]
-      anoi<-substring(nomecol,1,4)
-      anof<-substring(nomecol,8,12)
-      anoi<-as.integer(anoi)
-      anof<-as.integer(anof)
-      if(anof-anoi==transition_interval){
-        ret<-cbind(ret,tab[,i])
+    tab<-a
+    ncolu<-ncol(a)
+    ret<-c()
+    ret<-tab[,1:15]
+    if(!is.null(transition_interval)){
+      for(i in 16:ncolu){
+        nomecol<-colnames(a)[i]
+        anoi<-substring(nomecol,1,4)
+        anof<-substring(nomecol,8,12)
+        anoi<-as.integer(anoi)
+        anof<-as.integer(anof)
+        if(anof-anoi==transition_interval){
+          ret<-cbind(ret,tab[,i])
+        }
       }
     }
+    retorno<-ret
   }
-  retorno<-ret
-}
   return(retorno)
 }
