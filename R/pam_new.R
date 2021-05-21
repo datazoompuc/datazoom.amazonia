9bf9ff22fc51d5471b92d997714134b9a61b33fe
Isaac Newton <isaac_the_man@cambridge.org>
2021-05-21 08:14:59 -0300

    pam working at the state level

--- R/pam_new.R
+++ R/pam_new.R
@@ -1,26 +1,29 @@
 
 
-load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:2018, language = "pt") {
+load_pam = function(type=NULL,geo_level = "municipality", time_period = 2017:2018, language = "eng") {
 
   #############################
   ## Define Basic Parameters ##
   #############################
 
   param=list()
-  param$uf = c(12,27,13,16,29,23,32,52,21,31,50,51,15,25,26,22,41,33,24,11,14,43,42,28,35,17)
+  param$uf = c(11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,31,32,33,35,41,42,43,
+               50,51,52,53)
   param$time_period = time_period
 
   ## Dataset
 
   if (is.null(type)){stop('Missing Dataset!')}
 
-  if (as.numeric(type) == 1612){
-    param$type = 1612
+  if (as.numeric(type) == 1612){ ## This is a subset of all crops
+
+    param$type = as.numeric(type)
     param$data_name = 'Temporary Crops (Lavouras Temporárias)'
   }
 
-  if (as.numeric(type) == 1613){
-    param$type = 1613
+  if (as.numeric(type) == 1613){ ## This is a subset of all crops
+
+    param$type = as.numeric(type)
     param$data_name = 'Permanent Crops (Lavouras Permanente)'
   }
 
@@ -28,6 +31,13 @@ load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:201
     param$type = 5457
     param$data_name = 'All Crops (Lavouras)'
   }
+
+
+  if (as.numeric(type) %in% c(839,1001,1000,1002)){
+    param$type = as.numeric(type)
+    param$data_name = 'Crop with more than one harvest (Corn, Potato, Peanut or Beans)'
+  }
+
 
   ## Aggregation Level
 
@@ -45,7 +55,8 @@ load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:201
     y=param$time_period
   )
 
-  input = list(x = as.list(input_df$x),y = as.list(as.character(input_df$y)))
+  input_munic = list(x = as.list(input_df$x),y = as.list(as.character(input_df$y)))
+  input_other = list(x = as.list(as.character(param$time_period)))
 
   ###############
   ## Load Data ##
@@ -58,8 +69,18 @@ load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:201
   ## We use the purrr package (tidyverse equivalent of base apply functions) to run over the above grid
 
   if (geo_level %in% c('country','region','state')){
-    dat = input %>%
-      purrr::pmap(function(x,y) get_sidra_safe(param$type,geo=param$geo_reg,period = y))
+
+    base::message(base::cat('Downloading Data:',length(param$time_period),'API requests')) ## Show Message
+
+    ## Download
+
+    dat = input_other %>%
+      purrr::pmap(function(x) get_sidra_safe(param$type,geo=param$geo_reg,period = x))
+
+    ## Completion!
+
+    base::message(base::cat('Download Completed! Starting Data Processing...'))
+
   }
 
   if (geo_level == 'municipality'){
@@ -69,6 +90,10 @@ load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:201
 
   ## Capture Errors
 
+  ## Daniel Comment: The main point here is that the Sidra API has a limit for requests. Thus, for states with a large number
+  ## of municipalities the request may break. We used the purrr::safely to not break the loop (or map), but we will need to
+  ## deal with these problems in this step here. Now its under construction =)
+
   dat = lapply(dat,"[[", 1)
 
   boolean_downloaded = unlist(lapply(dat,is.data.frame))
@@ -77,22 +102,124 @@ load_pam = function(type=NULL,geo_level = 'municipality', time_period = 2017:201
 
   ## We need to check the municipalities in these UFs
 
+  #######################
+  ## Cleaning Function ##
+  #######################
+
+  clean_custom = function(var){
+    var = stringr::str_replace_all(string=var,pattern=' ',replacement='_')
+    var = stringr::str_replace_all(string=var,pattern='-',replacement='')
+    var = stringr::str_replace_all(string=var,pattern='ª',replacement='')
+    var = stringr::str_replace_all(string=var,pattern='\\(',replacement='')
+    var = stringr::str_replace_all(string=var,pattern='\\)',replacement='')
+    var = stringr::str_to_lower(string=var)
+    return(var)
+  }
+
   ## Binding Rows
 
-  dat = dat[boolean_downloaded] %>%
-    dplyr::bind_rows() %>%
-    janitor::clean_names() %>%
-    dplyr::mutate_all(function(x){iconv(x,to='ASCII')})
+  dat  = dat[boolean_downloaded] %>%
+          dplyr::bind_rows() %>%
+          tibble::as_tibble()
 
   ######################
   ## Data Enginnering ##
   ######################
 
   dat = dat %>%
-    dplyr::select(-c(nivel_territorial_codigo,nivel_territorial,unidade_de_medida_codigo))
+          janitor::clean_names() %>%
+          dplyr::mutate_all(function(var){stringi::stri_trans_general(str=var,id="Latin-ASCII")}) %>%
+          dplyr::mutate_all(clean_custom)
 
-  #table(dat$unidade_de_medida,dat$variavel)
+  dat = dat %>%
+    dplyr::select(-c(nivel_territorial_codigo,nivel_territorial,
+                     unidade_de_medida_codigo,variavel_codigo,
+                     ano_codigo)) %>%
+    dplyr::mutate(valor=as.numeric(valor))
 
-  ## Insert Code Similar to PPM
+  #########################################
+  ## Create Geographical Unit Identifier ##
+  #########################################
+
+  if(geo_level == 'country'){
+    dat$geo_id = dat$brasil
+    dat = dplyr::select(dat,-'brasil_codigo',-'brasil')
+  }
+  if (geo_level == 'region'){
+    dat$geo_id = dat$grande_regiao
+    dat = dplyr::select(dat,-'grande_regiao_codigo',-'grande_regiao')
+  }
+  if (geo_level == 'state'){
+    dat$geo_id = dat$unidade_da_federacao_codigo
+    dat = dplyr::select(dat,-'unidade_da_federacao_codigo',-'unidade_da_federacao')
+  }
+  if (geo_level == 'municipality'){
+    dat$geo_id = dat$municipio_codigo
+    dat = dplyr::select(dat,-'municipio',-'municipio_codigo')
+  }
+
+  #############################################
+  ## Adding Measure Information to Variables ##
+  #############################################
+
+    dat = dat %>%
+      dplyr::mutate(variavel = dplyr::case_when(
+                      (variavel == 'area_colhida') ~ 'area_colhida_ha',
+                      (variavel == 'area_plantada_ou_destinada_a_colheita') ~ 'area_plantada_ou_destinada_colheita_ha',
+                      (variavel == 'area_plantada') ~ 'area_plantada_ha',
+                      (variavel == 'area_destinada_a_colheita') ~ 'area_destinada_colheita_ha',
+                      (variavel == 'quantidade_produzida') ~ 'quant_prod_ton',
+                      (variavel == 'rendimento_medio_da_producao') ~ 'rend_medio_prod_kg_per_ha',
+                      ## Needs to be adjusted according to different currency systems in Brazil
+                      (variavel == 'valor_da_producao') ~ 'valor_da_producao_brl'
+                      )
+      )
+
+  ################################
+  ## Harmonizing Variable Names ##
+  ################################
+
+  if (param$type == 5457){
+    dat = dat %>%
+      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_temporarias_e_permanentes_codigo,
+                    produto_das_lavouras = produto_das_lavouras_temporarias_e_permanentes)
+  }
+
+  if (param$type == 1613){
+    dat = dat %>%
+      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_permanentes_codigo,
+                    produto_das_lavouras = produto_das_lavouras_permanentes)
+  }
+
+
+  if (param$type %in% c(839,1000,1001,1002,1612)){
+    dat = dat %>%
+      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_temporarias_codigo,
+                  produto_das_lavouras = produto_das_lavouras_temporarias)
+  }
+
+
+  #############################
+  ## Create Long Format Data ##
+  #############################
+
+  ## The Output is a tibble with unit and year identifiers + production and/or value of each item
+
+    dat = dat %>%
+      dplyr::select(-'unidade_de_medida',-'produto_das_lavouras_codigo') %>%
+      tidyr::pivot_wider(id_cols = c(geo_id,ano),
+                         names_from = variavel:produto_das_lavouras,
+                         values_from=valor,
+                         names_sep = '_',
+                         values_fn = sum,
+                         values_fill = 0) %>%
+      janitor::clean_names()
+
+
+  ##########################
+  ## Returning Data Frame ##
+  ##########################
+
+  return(dat)
 
 }
