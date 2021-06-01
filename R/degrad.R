84482282c25401b42721f171a9086dbaf0e41471
BrunoAlcantaraD <brunoalcanduarte@gmail.com>
2021-06-01 16:15:19 -0300

    Update degrad.R

--- R/degrad.R
+++ R/degrad.R
@@ -87,13 +87,6 @@ load_degrad <- function(source, space_aggregation = "municipality", time_aggrega
 #' load_degrad_raw("~/Downloads/degrad2016_final_shp/DEGRAD_2016_pol.shp")
 #' }
 load_degrad_raw <- function(source) {
-
-  ##############
-  ## Download ##
-  ##############
-
-  warning("This data source is experiencing problems. Execution may result in error.")
-
   # If source is a list of numbers (years), we retrieve data from INPE
   if (is.numeric(source)) {
     source <- purrr::map(source, webscrapping_degrad)
@@ -127,13 +120,8 @@ find_from_dir <- function(dir) {
 }
 
 treat_degrad_data <- function(df, space_aggregation, time_aggregation, language, geo_amazon, filter) {
-
-  ###################################################
-  ## Daniel Mental Note: I need to debug this data ##
-  ###################################################
-
   message("Processing data. This should take a few minutes.")
-  # Clean
+  # Lowers letters
   names(df) <- tolower(names(df))
 
   if (filter) {
