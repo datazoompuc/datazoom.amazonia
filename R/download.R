f352566bf829a9d19357b8d0a3977305b087fb4c
Daniel AC Barbosa <daniel.adrianocb@gmail.com>
2021-06-19 12:51:52 -0300

    putting sidra download to work

--- R/download.R
+++ R/download.R
@@ -165,7 +165,10 @@ download_br_data = function(data=NULL,dataset=NULL,source='ibge',geo_id='municip
 
 }
 
-sidra_download = function(sidra_code = NULL,time_period,geo_id = 'municipality'){
+sidra_download = function(sidra_code = NULL,year,geo_id = 'municipality'){
+
+  # Obs: Sometimes there are non-catched municipalieis - user should check on IBGE SIDRA
+  # f = geo %>% filter(!(code_muni %in% unique(dat_uf$`Município (Código)`)))
 
   # ----------------------------------------------------
 
@@ -184,38 +187,24 @@ sidra_download = function(sidra_code = NULL,time_period,geo_id = 'municipality')
   param = list()
 
   param$sidra_code = sidra_code
-  param$time_period = time_period
+  param$year = year
 
   if (geo_id == 'country'){param$geo_reg = 'Brazil'}
   if (geo_id == 'region'){param$geo_reg = 'Region'}
   if (geo_id == 'state'){param$geo_reg = 'State'}
   if (geo_id == 'municipality'){param$geo_reg = 'City'}
 
-  ##########################
-  ## Get Brazilian States ##
-  ##########################
+  ##################################
+  ## Get Geographical Information ##
+  ##################################
 
-  uf = geobr::read_state(year=2010,simplified=TRUE,showProgress = FALSE) %>%
-    sf::st_drop_geometry() %>%
-    dplyr::select(code_state) %>%
-    tibble::as_tibble()
+  geo = base::suppressMessages(geobr::lookup_muni('all'))
 
-  uf = uf$code_state
-
-  param$uf = uf
-
-
-  ################################################################
-  ## Create Grid with every possible combination of UF and Year ##
-  ################################################################
-
-  input_df = expand.grid(
-    geo=param$uf,
-    time=param$time_period
-  )
-
-  input_munic = list(geo = as.list(input_df$geo),time = as.list(as.character(input_df$time)))
-  input_other = list(geo = as.list(as.character(param$time_period)))
+  # uf = list('RO' = 11,'AC' = 12,'AM' = 13,'RR' = 14,'PA' = 15,'AP' = 16,'TO' = 17,
+  #           'MA' = 21,'PI' = 22,'CE' = 23,'RN' = 24,'PB' = 25,'PE' = 26,'AL' = 27,'SE' = 28,'BA' = 29,
+  #           'MG' = 31,'ES' = 32,'RJ' = 33,'SP' = 35,
+  #           'PR' = 41,'SC' = 42,'RS' = 43,
+  #           'MS' = 50,'MT' = 51,'GO' = 52,'DF' = 53)
 
   ###############
   ## Load Data ##
@@ -223,210 +212,244 @@ sidra_download = function(sidra_code = NULL,time_period,geo_id = 'municipality')
 
   get_sidra_safe = purrr::safely(sidrar::get_sidra)
 
-  ## We will need to check for the ones not considered in the loop afterwards
-
-  ## We use the purrr package (tidyverse equivalent of base apply functions) to run over the above grid
 
   if (param$geo_reg %in% c('Brazil','Region','State')){
 
-    ## Download all units together
-
-    base::message(base::cat('Downloading Data:',length(param$time_period),'API requests')) ## Show Message
+    base::message(base::cat('Downloading Data at the',param$geo_reg,'level')) ## Show Message
 
     ## Download
 
-    dat = input_other %>%
-      purrr::pmap(function(geo) get_sidra_safe(param$type,geo=param$geo_reg,period = geo))
+    dat = get_sidra_safe(x = param$sidra_code,
+                         geo = param$geo_reg,
+                         period = as.character(param$year))
 
-    ## Completion!
-
-    base::message(base::cat('Download Completed! Starting Data Processing...'))
+    base::message(base::cat('Download Succesfully Completed!'))
 
   }
 
   if (param$geo_reg == 'City'){
 
-    # ----------------------------
+    ##############################
+    ## Download at the UF Level ##
+    ##############################
 
-    ## 1) Filter by UF
+    uf_list = geo %>%
+      dplyr::select(code_state) %>%
+      unlist() %>%
+      unique() %>%
+      as.list()
 
-    ## 2) Filter by MesoRegion
+    names(uf_list) = uf_list
 
-    ## 3) Filter by MicroRegion
+    dat_raw_uf = purrr::map(uf_list,function(uf){
 
-    # ----------------------------
+        base::message(base::cat(which(uf == uf_list),'in',length(uf_list),'states...\n'))
 
-    #base::message(base::cat('Downloading Data:',length(param$time_period),'API requests'))
-    base::message(base::cat('Attempting to download data at the state level:',length(param$uf),'API requests'))
+        suppressMessages(
+          get_sidra_safe(x = param$sidra_code,
+                         geo = param$geo_reg,
+                         period = as.character(param$year),
+                         geo.filter = list("State" = uf))
+        )
 
-    ## Download UF
-
-    dat_uf = input_munic %>%
-      purrr::pmap(function(geo,time){
-
-        base::suppressMessages(get_sidra_safe(x = param$sidra_code,
-                                                    geo = param$geo_reg, # Municipality Level - City
-                                                    period = time,
-                                                    geo.filter = list("State" = geo)))
-        base::message(base::cat(which(geo == unique(input_df$geo)),'of',length(unique(input_df$geo))))
-      })
-
-    dat_uf = base::lapply(dat_uf,"[[", 1)
-
-    missed_uf = param$uf[which(unlist(lapply(dat_uf,is.data.frame)) == FALSE)]
-
-    if (length(missed_uf) > 0){
-      base::message(base::cat(length(missed_uf),
-                              'missed API requests at the state level. Moving to more disaggregated requests...'))
-    }
-    if (length(missed_uf) == 0){base::message(base::cat('Download Succesfully Completed!'))}
-
-    dat_uf = dat_uf[unlist(lapply(dat_uf,is.data.frame))] %>% ## Filter for only found dataframes
-      dplyr::bind_rows() %>%
-      tibble::as_tibble()
-
-    ###################
-    ## Run if needed ##
-    ###################
-
-    if (length(missed_uf) > 0){
-
-      # ## Meso Region
-      #
-      # meso = geobr::read_meso_region(code_meso = missed_uf,year=2010,simplified=TRUE) %>%
-      #   sf::st_drop_geometry() %>%
-      #   dplyr::select(code_state,code_meso) %>%
-      #   tibble::as_tibble()
-      #
-      # input_meso = expand.grid(
-      #   geo=meso$code_meso,
-      #   time=param$time_period
-      # )
-      #
-      # input_meso = list(geo = as.list(input_meso$geo),time = as.list(as.character(input_meso$time)))
-      #
-      # dat_meso = input_meso %>%
-      #   purrr::pmap(function(geo,time) get_sidra_safe(x = param$sidra_code,
-      #                                                 geo = param$geo_reg, # Municipality Level - City
-      #                                                 period = time,
-      #                                                 geo.filter = list("MesoRegion" = geo))) %>%
-      #   base::lapply("[[", 1)
-      #
-      # missed_meso = meso[which(unlist(lapply(dat_meso,is.data.frame)) == FALSE),]
-      #
-      # dat_meso = dat_meso[unlist(lapply(dat_meso,is.data.frame))] %>% ## Filter for only found dataframes
-      #   dplyr::bind_rows() %>%
-      #   tibble::as_tibble()
-
-
-
-    }
-
-    #if (length(missed_meso) > 0){
-
-    if (length(missed_uf) > 0){
-
-      ## Micro Region
-
-      micro = geobr::read_micro_region(code_micro = missed_uf,year=2010,simplified=TRUE) %>%
-        sf::st_drop_geometry() %>%
-        dplyr::select(code_state,code_micro) %>%
-        tibble::as_tibble()
-
-      base::message(base::cat('Attempting to download data at the Micro Region Level:\n',length(micro$code_micro),'API requests'))
-
-      input_micro = expand.grid(
-        geo=micro$code_micro,
-        time=param$time_period
-      )
-
-      input_micro = list(geo = as.list(input_micro$geo),time = as.list(as.character(input_micro$time)))
-
-      dat_micro = input_micro %>%
-        purrr::pmap(function(geo,time){
-
-          base::suppressMessages(get_sidra_safe(x = param$sidra_code,
-                                                geo = param$geo_reg, # Municipality Level - City
-                                                period = time,
-                                                geo.filter = list("MicroRegion" = geo))
-          )
-          base::message(base::cat(which(geo == micro$code_micro),'of',length(micro$code_micro)))
-        }) %>%
-        base::lapply("[[", 1)
-
-      missed_micro = micro[which(unlist(lapply(dat_micro,is.data.frame)) == FALSE),]
-
-      if (length(missed_micro) > 0){
-        base::message(base::cat(length(missed_micro),
-                                'missed API requests at the Micro Region level. Please report this problem to package developers...'))
       }
-      if (length(missed_micro) == 0){base::message(base::cat('Download Succesfully Completed!'))}
+    )
 
-      dat_micro = dat_micro[unlist(lapply(dat_micro,is.data.frame))] %>% ## Filter for only found dataframes
-        dplyr::bind_rows() %>%
-        tibble::as_tibble()
+    dat_mod_uf = base::lapply(dat_raw_uf,"[[", 1)
 
-      dat = dplyr::bind_rows(dat_uf,dat_micro)
+    dat_uf = dat_mod_uf[unlist(lapply(dat_mod_uf,is.data.frame))] %>% ## Filter for only found dataframes
+      dplyr::bind_rows() %>%
+      tibble::as_tibble() %>%
+      janitor::clean_names()
 
-      rm(dat_uf,dat_micro)
+    ###########################################
+    ## Checking for Completeness of Download ##
+    ###########################################
 
+    missed_uf = dat_mod_uf[!unlist(lapply(dat_mod_uf,is.data.frame))] %>% names()
 
+    rm(dat_mod_uf)
 
-      # micro = geobr::read_micro_region(code_micro = unique(missed_meso$code_state),year=2010,simplified=TRUE) %>%
-      #   sf::st_drop_geometry() %>%
-      #   dplyr::select(code_state,code_micro) %>%
-      #   tibble::as_tibble()
-      #
-      # input_micro = expand.grid(
-      #   geo=micro$code_micro,
-      #   time=param$time_period
-      # )
-      #
-      # input_micro = list(geo = as.list(input_micro$geo),time = as.list(as.character(input_micro$time)))
-      #
-      # dat_micro = input_micro %>%
-      #   purrr::pmap(function(geo,time) get_sidra_safe(x = param$sidra_code,
-      #                                                 geo = param$geo_reg, # Municipality Level - City
-      #                                                 period = time,
-      #                                                 geo.filter = list("microRegion" = geo))) %>%
-      #   base::lapply("[[", 1)
-      #
-      # missed_micro = micro[which(unlist(lapply(dat_micro,is.data.frame)) == FALSE),]
-      #
-      # dat_micro = dat_micro[unlist(lapply(dat_micro,is.data.frame))] %>% ## Filter for only found dataframes
-      #   dplyr::bind_rows() %>%
-      #   tibble::as_tibble()
+    if (length(missed_uf) > 0){
+      base::message(base::cat('Download at the State Level Completed!',length(missed_uf),'failed.\n',
+                                                       'Attempting to Download at the MesoRegion Level...'))
+    } else if (length(missed_uf) == 0){
 
+      base::message(base::cat('Download Succesfully Completed!'))
 
+      return(dat_uf)
+
+      }
+
+    #################
+    ## Meso Region ##
+    #################
+
+    if (length(missed_uf) > 0){
+
+    geo_meso = geo %>%
+      dplyr::filter(code_state %in% missed_uf)
+
+    meso_reg_list = geo_meso %>%
+      dplyr::select(code_meso) %>%
+      unlist() %>%
+      unique() %>%
+      as.list()
+
+    names(meso_reg_list) = meso_reg_list
+
+    dat_raw_meso = purrr::map(meso_reg_list,function(meso_reg){
+
+      base::message(base::cat(which(meso_reg == meso_reg_list),'in',length(meso_reg_list),'meso regions...\n'))
+
+      base::suppressMessages(
+        get_sidra_safe(x = param$sidra_code,
+                      geo = param$geo_reg,
+                      period = as.character(param$year),
+                      geo.filter = list("MesoRegion" = meso_reg))
+        )
+
+      }
+    )
+
+    dat_mod_meso = base::lapply(dat_raw_meso,"[[", 1)
+
+    dat_meso = dat_mod_meso[unlist(lapply(dat_mod_meso,is.data.frame))] %>% ## Filter for only found dataframes
+      dplyr::bind_rows() %>%
+      tibble::as_tibble() %>%
+      janitor::clean_names()
+
+    dat_uf = dat_uf %>%
+      dplyr::bind_rows(dat_meso)
+
+    ###########################################
+    ## Checking for Completeness of Download ##
+    ###########################################
+
+    missed_meso = dat_mod_meso[!unlist(lapply(dat_mod_meso,is.data.frame))] %>% names()
+
+    rm(dat_mod_meso)
+
+    if (length(missed_meso) > 0){
+      base::message(base::cat('Download at the Meso Region Level Completed!',length(missed_meso),'failed.\n',
+                              'Attempting to Download at the MicroRegion Level...'))
+    } else if (length(missed_meso) == 0){
+
+      base::message(base::cat('Download Succesfully Completed!'))
+
+      return(dat_uf)
 
     }
 
-    ################
-    ## Next Steps ##
-    ################
+    ##################
+    ## Micro Region ##
+    ##################
 
-    if (length(missed_uf == 0)){dat = dat_uf}
+    if (length(missed_meso) > 0){
+
+    geo_micro = geo %>%
+      dplyr::filter(code_meso %in% missed_meso)
+
+    micro_reg_list = geo_micro %>%
+      dplyr::select(code_micro) %>%
+      unlist() %>%
+      unique() %>%
+      as.list()
+
+    names(micro_reg_list) = micro_reg_list
+
+    dat_raw_micro = purrr::map(micro_reg_list,function(micro_reg){
+
+      base::message(base::cat(which(micro_reg == micro_reg_list),'in',length(micro_reg_list),'micro regions...\n'))
+
+      base::suppressMessages(
+        get_sidra_safe(x = param$sidra_code,
+                      geo = param$geo_reg,
+                      period = as.character(param$year),
+                      geo.filter = list("MicroRegion" = micro_reg))
+      )
+    }
+    )
+
+    dat_mod_micro = base::lapply(dat_raw_micro,"[[", 1)
+
+    dat_micro = dat_mod_micro[unlist(lapply(dat_mod_micro,is.data.frame))] %>% ## Filter for only found dataframes
+      dplyr::bind_rows() %>%
+      tibble::as_tibble() %>%
+      janitor::clean_names()
+
+    dat_uf = dat_uf %>%
+      dplyr::bind_rows(dat_micro)
+
+    ###########################################
+    ## Checking for Completeness of Download ##
+    ###########################################
+
+    missed_micro = dat_mod_micro[!unlist(lapply(dat_mod_micro,is.data.frame))] %>% names()
+
+    rm(dat_mod_micro)
+
+    if (length(missed_micro) > 0){
+      base::message(base::cat(length(missed_micro),
+                              'missed API requests at the Micro Region level.
+                              Please report this problem to package developers...'))
+    }
+    if (length(missed_micro) == 0){
+
+      base::message(base::cat('Download Succesfully Completed!'))
+
+      return(dat_uf)
+
+      }
+
+    } # End of if Meso
+
+    } # End of if Uf
+
+    } # End of If - Download at the Municipality Level
 
 
-  }
 
-  #################
-  ## Downloading ##
-  #################
+}
 
-  ## If data is requested:
-  ## i) At the country, region or state level, we just download all the units together.
-  ## ii) At the municipality level, we attempt to download all the municipalities in a given
-  ## state, mesoregion and microregion, respectively
+external_download = function(data){
+
+  ## Get Download Link
+
+  ###########################
+  ## Initiate Curl Process ##
+  ###########################
+
+  ## Defining URL from TerraBrasilis
+
+  url <- paste0("http://terrabrasilis.dpi.inpe.br/file-delivery/download/deter-", source, "/shape")
+
+  ## Define Empty Directory and Files For Download
+
+  dir <- tempdir()
+
+  temp <- tempfile(fileext = ".zip", tmpdir = dir)
+
+  ## Extraction through Curl Requests
+  ## Investigate a bit more on how Curl Requests work
+
+  proc <- RCurl::CFILE(temp, mode = "wb")
+
+  RCurl::curlPerform(url = url, writedata = proc@ref, noprogress = FALSE)
+
+  RCurl::close(proc)
+
+  ## This Data Opening Part Should Include the .Shp, not DBF
+
+  utils::unzip(temp, exdir = dir)
+
+  df = sf::read_sf(paste(dir, "deter_public.shp", sep = "/"))
 
 
 
 
 
 }
-
-external_download = function()
-
 
 terrabrasilis_download = function(geo = 'amazon_biome',type='cumulative_deforestation'){
 
@@ -468,6 +491,8 @@ terrabrasilis_download = function(geo = 'amazon_biome',type='cumulative_deforest
 }
 
 datasets_link = function(){
+
+  ## Add file type at the end in order to set the Curl Process
 
   link = tibble::tribble(~survey,~dataset,~sidra_code,~available_time,~available_geo,~link,
                          'PAM-IBGE','pam_all_crops',5457,NA,NA,'https://sidra.ibge.gov.br/pesquisa/pam/tabelas',
