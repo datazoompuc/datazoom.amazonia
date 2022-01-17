a22a4ac88fa31cd5ce700d13fa676152dd85e56b
IgorRigolon <77173467+IgorRigolon@users.noreply.github.com>
2022-01-17 12:22:56 -0300

    Passando no check

--- R/cipo.R
+++ R/cipo.R
@@ -24,33 +24,46 @@
 load_cipo <- function(dataset = "brazilian_actors",
                       search = ""){
 
-param <- list()
+  ##############################
+  ## Binding Global Variables ##
+  ##############################
 
-param$dataset <- dataset
-param$search <- clean_text(search)
+  aux <- NULL
 
-# spreadsheet embedded into the page is sourced from google sheets
+  #############################
+  ## Define Basic Parameters ##
+  #############################
 
-  if (dataset == "brazilian_actors"){
+  param <- list()
+
+  param$dataset <- dataset
+  param$search <- clean_text(search)
+
+  # spreadsheet embedded into the page is sourced from google sheets
+
+  if (param$dataset == "brazilian_actors"){
     url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTpRIu-paL_8rtXLpiT-kCTJRa2Tf_jCCPZxZBc3sjCwMHL8mkrhG2eqVeeIdWkxLTUKPru5uYAWG6g/"
     skip_rows <- 2
   }
-  else if (dataset == "international_cooperation"){
+  else if (param$dataset == "international_cooperation"){
     url <- "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vSpyBina4qr3GG-5ZlKW8_fjQwgIP3lq5lxanpO5_bUZenCVFO6N-WrF3bTkpokVzNVpRnob9Jhn8qe/"
     skip_rows <- 0
   }
-  else if (dataset == "forest_governance"){
+  else if (param$dataset == "forest_governance"){
     url <- "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vTpnO9DEiy1mMRwBI5jAzBbYhFVBlcsX4TNRZyoDYBNUhEPZcLviexaynCJfY3JC-CCBGy00-Fs3jxu/"
     skip_rows <- 0
+  }
+  else{
+    stop("Invalid dataset")
   }
 
 url <- paste0(url, "pub?output=csv")
 
 df <- readr::read_csv(url, skip = skip_rows) %>%
   tidyr::unite(aux, sep = " ", remove = FALSE) %>%
-  mutate(across(aux, clean_text)) %>%
-  filter(across(aux, ~ str_detect(., search))) %>%
-  select(-aux)
+  dplyr::mutate(dplyr::across(aux, clean_text)) %>%
+  dplyr::filter(dplyr::across(aux, ~ str_detect(., param$search))) %>%
+  dplyr::select(-aux)
 
 df
 
@@ -59,5 +72,5 @@ df
 clean_text <- function(text){
    text %>%
   tolower() %>%
-  stringi::stri_trans_general(., id = "Latin-ASCII")
+  stringi::stri_trans_general(id = "Latin-ASCII")
 }
