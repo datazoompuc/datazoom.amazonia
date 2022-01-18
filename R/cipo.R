2c52f44f9c3346d1ac174b33655bbcfbe05a7280
IgorRigolon <77173467+IgorRigolon@users.noreply.github.com>
2022-01-18 17:03:22 -0300

    Usando styler

--- R/cipo.R
+++ R/cipo.R
@@ -14,15 +14,19 @@
 #' brazilian_actors <- load_cipo(dataset = "brazilian_actors")
 #'
 #' # searching only for entries containing IBAMA
-#' actors_ibama <- load_cipo(dataset = "brazilian_actors",
-#'                          search = "ibama")
+#' actors_ibama <- load_cipo(
+#'   dataset = "brazilian_actors",
+#'   search = "ibama"
+#' )
 #'
 #' # entries containing IBAMA or FUNAI
-#' actors_ibama <- load_cipo(dataset = "brazilian_actors",
-#'                          search = "ibama|funai")
+#' actors_ibama <- load_cipo(
+#'   dataset = "brazilian_actors",
+#'   search = "ibama|funai"
+#' )
 #' }
 load_cipo <- function(dataset = "brazilian_actors",
-                      search = ""){
+                      search = "") {
 
   ##############################
   ## Binding Global Variables ##
@@ -41,36 +45,34 @@ load_cipo <- function(dataset = "brazilian_actors",
 
   # spreadsheet embedded into the page is sourced from google sheets
 
-  if (param$dataset == "brazilian_actors"){
+  if (param$dataset == "brazilian_actors") {
     url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTpRIu-paL_8rtXLpiT-kCTJRa2Tf_jCCPZxZBc3sjCwMHL8mkrhG2eqVeeIdWkxLTUKPru5uYAWG6g/"
     skip_rows <- 2
-  }
-  else if (param$dataset == "international_cooperation"){
+  } else if (param$dataset == "international_cooperation") {
     url <- "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vSpyBina4qr3GG-5ZlKW8_fjQwgIP3lq5lxanpO5_bUZenCVFO6N-WrF3bTkpokVzNVpRnob9Jhn8qe/"
     skip_rows <- 0
-  }
-  else if (param$dataset == "forest_governance"){
+  } else if (param$dataset == "forest_governance") {
     url <- "https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vTpnO9DEiy1mMRwBI5jAzBbYhFVBlcsX4TNRZyoDYBNUhEPZcLviexaynCJfY3JC-CCBGy00-Fs3jxu/"
     skip_rows <- 0
-  }
-  else{
+  } else {
     stop("Invalid dataset")
   }
 
-url <- paste0(url, "pub?output=csv")
+  url <- paste0(url, "pub?output=csv")
 
-df <- readr::read_csv(url, skip = skip_rows) %>%
-  tidyr::unite(aux, sep = " ", remove = FALSE) %>%
-  dplyr::mutate(dplyr::across(aux, clean_text)) %>%
-  dplyr::filter(dplyr::across(aux, ~ stringr::str_detect(., param$search))) %>%
-  dplyr::select(-aux)
+  ## Dataset
 
-df
+  df <- readr::read_csv(url, skip = skip_rows) %>%
+    tidyr::unite(aux, sep = " ", remove = FALSE) %>%
+    dplyr::mutate(dplyr::across(aux, clean_text)) %>%
+    dplyr::filter(dplyr::across(aux, ~ stringr::str_detect(., param$search))) %>%
+    dplyr::select(-aux)
 
+  df
 }
 
-clean_text <- function(text){
-   text %>%
-  tolower() %>%
-  stringi::stri_trans_general(id = "Latin-ASCII")
+clean_text <- function(text) {
+  text %>%
+    tolower() %>%
+    stringi::stri_trans_general(id = "Latin-ASCII")
 }
