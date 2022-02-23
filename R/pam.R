d94cb5266dc293c2eafa89d7b41285bd7f726e33
IgorRigolon <77173467+IgorRigolon@users.noreply.github.com>
2022-02-23 01:10:54 -0300

    Restoring PAM update
    * Restoring previous version
    
    * Documentation update

--- R/pam.R
+++ R/pam.R
@@ -16,15 +16,16 @@
 #'
 #' @examples \dontrun{
 #' # download state raw data from 2012 for all crops
-#' pam_all_crops <- load_pam(dataset = 'pam_all_crops',
-#'                           raw_data = TRUE,
-#'                           geo_level = 'state',
-#'                           time_period = 2012)
+#' pam_all_crops <- load_pam(
+#'   dataset = "pam_all_crops",
+#'   raw_data = TRUE,
+#'   geo_level = "state",
+#'   time_period = 2012
+#' )
 #' }
-
-load_pam = function(dataset = NULL, raw_data,
-                    geo_level, time_period,
-                    language = "eng") {
+load_pam <- function(dataset = NULL, raw_data,
+                     geo_level, time_period,
+                     language = "eng") {
 
   ## Translation is only made through collapsing at the end
   # - What if we wanted to deliver raw data?
@@ -41,7 +42,7 @@ load_pam = function(dataset = NULL, raw_data,
 
   sidra_code <- nivel_territorial_codigo <- nivel_territorial <- nivel_territorial_codigo <- NULL
   unidade_de_medida_codigo <- variavel_codigo <- ano_codigo <- valor <- NULL
-  produto_das_lavouras_temporarias_e_permanentes_codigo  <- NULL
+  produto_das_lavouras_temporarias_e_permanentes_codigo <- NULL
   produto_das_lavouras_temporarias_e_permanentes <- NULL
   produto_das_lavouras_permanentes_codigo <- NULL
   produto_das_lavouras_permanentes <- NULL
@@ -55,54 +56,64 @@ load_pam = function(dataset = NULL, raw_data,
   ## Define Basic Parameters ##
   #############################
 
-  param=list()
-  param$dataset = dataset
-  param$geo_level = geo_level
-  param$time_period = time_period
-  param$language = language
+  param <- list()
+  param$dataset <- dataset
+  param$geo_level <- geo_level
+  param$time_period <- time_period
+  param$language <- language
 
-  if (!is.numeric(param$dataset)){
-    param$code = datasets_link() %>%
+  if (!is.numeric(param$dataset)) {
+    param$code <- datasets_link() %>%
       dplyr::filter(dataset == param$dataset) %>%
       dplyr::select(sidra_code) %>%
       unlist() %>%
       as.numeric()
-  } else {param$code = param$dataset}
+  } else {
+    param$code <- param$dataset
+  }
 
   ## Check if year is acceptable
 
-  year_check = datasets_link() %>%
+  year_check <- datasets_link() %>%
     dplyr::filter(dataset == param$dataset) %>%
     dplyr::select(available_time) %>%
-    unlist() %>% as.character() %>%
-    stringr::str_split(pattern = '-') %>%
-    unlist() %>% as.numeric()
+    unlist() %>%
+    as.character() %>%
+    stringr::str_split(pattern = "-") %>%
+    unlist() %>%
+    as.numeric()
 
-  if (min(time_period) < year_check[1]){stop('Provided time period less than supported. Check documentation for time availability.')}
-  if (max(time_period) > year_check[2]){stop('Provided time period greater than supported. Check documentation for time availability.')}
+  if (min(time_period) < year_check[1]) {
+    stop("Provided time period less than supported. Check documentation for time availability.")
+  }
+  if (max(time_period) > year_check[2]) {
+    stop("Provided time period greater than supported. Check documentation for time availability.")
+  }
 
 
 
 
   ## Dataset
 
-  if (is.null(dataset)){stop('Missing Dataset!')}
-
-  if (param$code == 1612){ ## This is a subset of all crops
-    param$data_name = 'Temporary Crops (Lavouras Temporarias)'
+  if (is.null(dataset)) {
+    stop("Missing Dataset!")
   }
 
-  if (param$code == 1613){ ## This is a subset of all crops
-    param$data_name = 'Permanent Crops (Lavouras Permanente)'
+  if (param$code == 1612) { ## This is a subset of all crops
+    param$data_name <- "Temporary Crops (Lavouras Temporarias)"
   }
 
-  if (param$code == 5457){
-    param$data_name = 'All Crops (Lavouras)'
+  if (param$code == 1613) { ## This is a subset of all crops
+    param$data_name <- "Permanent Crops (Lavouras Permanente)"
+  }
+
+  if (param$code == 5457) {
+    param$data_name <- "All Crops (Lavouras)"
   }
 
 
-  if (param$code %in% c(839,1001,1000,1002)){
-    param$data_name = 'Crop with more than one harvest (Corn, Potato, Peanut or Beans)'
+  if (param$code %in% c(839, 1001, 1000, 1002)) {
+    param$data_name <- "Crop with more than one harvest (Corn, Potato, Peanut or Beans)"
   }
 
   ##############
@@ -112,115 +123,121 @@ load_pam = function(dataset = NULL, raw_data,
   # We need to show year that is being downloaded as well
   # Heavy Datasets may take several minutes
 
-  dat = as.list(as.character(param$time_period)) %>%
-    purrr::map(function(year_num){
-      #suppressMessages(
-        sidra_download(sidra_code = param$code,year = year_num,geo_level = param$geo_level)
-        #)
-      }) %>%
+  dat <- as.list(as.character(param$time_period)) %>%
+    purrr::map(function(year_num) {
+      base::message(base::cat('Dowloading', year_num, 'data'))
+      # suppressMessages(
+      sidra_download(sidra_code = param$code, year = year_num, geo_level = param$geo_level)
+      # )
+    }) %>%
     dplyr::bind_rows() %>%
     tibble::as_tibble()
 
   ## Return Raw Data
 
-  if (raw_data == TRUE){return(dat)}
+  if (raw_data == TRUE) {
+    return(dat)
+  }
 
   ######################
   ## Data Enginnering ##
   ######################
 
-  dat = dat %>%
-          janitor::clean_names() %>%
-          dplyr::mutate_all(function(var){stringi::stri_trans_general(str=var,id="Latin-ASCII")})# %>%
-          # dplyr::mutate_all(clean_custom)
+  dat <- dat %>%
+    janitor::clean_names() %>%
+    dplyr::mutate_all(function(var) {
+      stringi::stri_trans_general(str = var, id = "Latin-ASCII")
+    }) # %>%
+  # dplyr::mutate_all(clean_custom)
 
-  dat = dat %>%
-    dplyr::select(-c(nivel_territorial_codigo,nivel_territorial,ano_codigo)) %>%
-    dplyr::mutate(valor=as.numeric(valor))
+  dat <- dat %>%
+    dplyr::select(-c(nivel_territorial_codigo, nivel_territorial, ano_codigo)) %>%
+    dplyr::mutate(valor = as.numeric(valor))
 
   ## Only Keep Valid Observations
 
-  dat = dat %>%
+  dat <- dat %>%
     dplyr::filter(!is.na(valor))
 
   #########################################
   ## Create Geographical Unit Identifier ##
   #########################################
 
-  if(geo_level == 'country'){
-    dat$geo_id = dat$brasil
-    dat = dplyr::select(dat,-'brasil_codigo',-'brasil')
+  if (geo_level == "country") {
+    dat$geo_id <- dat$brasil
+    dat <- dplyr::select(dat, -"brasil_codigo", -"brasil")
   }
-  if (geo_level == 'region'){
-    dat$geo_id = dat$grande_regiao
-    dat = dplyr::select(dat,-'grande_regiao_codigo',-'grande_regiao')
+  if (geo_level == "region") {
+    dat$geo_id <- dat$grande_regiao
+    dat <- dplyr::select(dat, -"grande_regiao_codigo", -"grande_regiao")
   }
-  if (geo_level == 'state'){
-    dat$geo_id = dat$unidade_da_federacao_codigo
-    dat = dplyr::select(dat,-'unidade_da_federacao_codigo',-'unidade_da_federacao')
+  if (geo_level == "state") {
+    dat$geo_id <- dat$unidade_da_federacao_codigo
+    dat <- dplyr::select(dat, -"unidade_da_federacao_codigo", -"unidade_da_federacao")
   }
-  if (geo_level == 'municipality'){
-    dat$geo_id = dat$municipio_codigo
-    dat = dplyr::select(dat,-'municipio',-'municipio_codigo')
+  if (geo_level == "municipality") {
+    dat$geo_id <- dat$municipio_codigo
+    dat <- dplyr::select(dat, -"municipio", -"municipio_codigo")
   }
 
   ################################
   ## Harmonizing Variable Names ##
   ################################
 
-  dat = dat %>%
-    dplyr::select(-unidade_de_medida,-unidade_de_medida_codigo)
+  dat <- dat %>%
+    dplyr::select(-unidade_de_medida, -unidade_de_medida_codigo)
 
   ## Change Variable Names for Common Across Datasets
 
-  if (param$code == 5457){
-    dat = dat %>%
-      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_temporarias_e_permanentes_codigo,
-                    produto_das_lavouras = produto_das_lavouras_temporarias_e_permanentes)
+  if (param$code == 5457) {
+    dat <- dat %>%
+      dplyr::rename(
+        produto_das_lavouras_codigo = produto_das_lavouras_temporarias_e_permanentes_codigo,
+        produto_das_lavouras = produto_das_lavouras_temporarias_e_permanentes
+      )
   }
 
-  if (param$code == 1613){
-    dat = dat %>%
-      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_permanentes_codigo,
-                    produto_das_lavouras = produto_das_lavouras_permanentes)
+  if (param$code == 1613) {
+    dat <- dat %>%
+      dplyr::rename(
+        produto_das_lavouras_codigo = produto_das_lavouras_permanentes_codigo,
+        produto_das_lavouras = produto_das_lavouras_permanentes
+      )
   }
 
 
-  if (param$code %in% c(839,1000,1001,1002,1612)){
-    dat = dat %>%
-      dplyr::rename(produto_das_lavouras_codigo = produto_das_lavouras_temporarias_codigo,
-                  produto_das_lavouras = produto_das_lavouras_temporarias)
+  if (param$code %in% c(839, 1000, 1001, 1002, 1612)) {
+    dat <- dat %>%
+      dplyr::rename(
+        produto_das_lavouras_codigo = produto_das_lavouras_temporarias_codigo,
+        produto_das_lavouras = produto_das_lavouras_temporarias
+      )
   }
 
   ## Translation
 
-  if (language == 'pt'){
-
-    dat = dat %>%
+  if (language == "pt") {
+    dat <- dat %>%
       dplyr::mutate(variavel = dplyr::case_when(
-        (variavel_codigo == '112') ~ 'rend_medio', # Rendimento medio da producao
-        (variavel_codigo == '214') ~ 'quant', # Quantidade produzida
-        (variavel_codigo == '215') ~ 'valor', # Valor da producao
-        (variavel_codigo == '216') ~ 'area_colhida', # Area colhida
-        (variavel_codigo == '8331') ~ 'area_plantada_dest_colheita', # Area plantada ou destinada a colheita,
-        (variavel_codigo == '109') ~ 'area_plantada' # Area plantada,
-        )
-      )
+        (variavel_codigo == "112") ~ "rend_medio", # Rendimento medio da producao
+        (variavel_codigo == "214") ~ "quant", # Quantidade produzida
+        (variavel_codigo == "215") ~ "valor", # Valor da producao
+        (variavel_codigo == "216") ~ "area_colhida", # Area colhida
+        (variavel_codigo == "8331") ~ "area_plantada_dest_colheita", # Area plantada ou destinada a colheita,
+        (variavel_codigo == "109") ~ "area_plantada" # Area plantada,
+      ))
   }
 
-  if (language == 'eng'){
-
-    dat = dat %>%
+  if (language == "eng") {
+    dat <- dat %>%
       dplyr::mutate(variavel = dplyr::case_when(
-        (variavel_codigo == '112') ~ 'avg_yield', # Rendimento medio da producao
-        (variavel_codigo == '214') ~ 'quant', # Quantidade produzida
-        (variavel_codigo == '215') ~ 'value', # Valor da producao
-        (variavel_codigo == '216') ~ 'harvested_area', # Area colhida
-        (variavel_codigo == '8331') ~ 'planted_to_harvest_area', # Area plantada ou destinada a colheita
-        (variavel_codigo == '109') ~ 'planted_area' # Area plantada,
-      )
-      )
-
+        (variavel_codigo == "112") ~ "avg_yield", # Rendimento medio da producao
+        (variavel_codigo == "214") ~ "quant", # Quantidade produzida
+        (variavel_codigo == "215") ~ "value", # Valor da producao
+        (variavel_codigo == "216") ~ "harvested_area", # Area colhida
+        (variavel_codigo == "8331") ~ "planted_to_harvest_area", # Area plantada ou destinada a colheita
+        (variavel_codigo == "109") ~ "planted_area" # Area plantada,
+      ))
   }
 
 
@@ -230,15 +247,17 @@ load_pam = function(dataset = NULL, raw_data,
 
   ## The Output is a tibble with unit and year identifiers + production and/or value of each item
 
-  dat = dat %>%
-    dplyr::select(-'produto_das_lavouras') %>%
-    dplyr::arrange(produto_das_lavouras_codigo,variavel) %>%
-    tidyr::pivot_wider(id_cols = c(geo_id,ano),
-                       names_from = variavel:produto_das_lavouras_codigo,
-                       values_from=valor,
-                       names_sep = '_V',
-                       values_fn = sum,
-                       values_fill = NA) %>%
+  dat <- dat %>%
+    dplyr::select(-"produto_das_lavouras") %>%
+    dplyr::arrange(produto_das_lavouras_codigo, variavel) %>%
+    tidyr::pivot_wider(
+      id_cols = c(geo_id, ano),
+      names_from = variavel:produto_das_lavouras_codigo,
+      values_from = valor,
+      names_sep = "_V",
+      values_fn = sum,
+      values_fill = NA
+    ) %>%
     janitor::clean_names()
 
   ########################
@@ -246,9 +265,8 @@ load_pam = function(dataset = NULL, raw_data,
   ########################
 
 
-  if (language == 'eng'){
-
-    dat = dat %>%
+  if (language == "eng") {
+    dat <- dat %>%
       dplyr::rename(year = ano)
   }
 
@@ -261,61 +279,53 @@ load_pam = function(dataset = NULL, raw_data,
     x
   }
 
-  label_data_eng = function(df,cols,dic){
+  label_data_eng <- function(df, cols, dic) {
+    label_value <- as.character(dic[dic$var_code == cols, "var_eng"])
 
-    label_value = as.character(dic[dic$var_code == cols,'var_eng'])
-
-    df = df %>%
-      dplyr::mutate_at(dplyr::vars(tidyr::matches(cols)),
-                       ~ labelled(.,as.character(dic[dic$var_code == cols,'var_eng']))
+    df <- df %>%
+      dplyr::mutate_at(
+        dplyr::vars(tidyr::matches(cols)),
+        ~ labelled(., as.character(dic[dic$var_code == cols, "var_eng"]))
       )
 
     return(df)
-
   }
 
-  label_data_pt = function(df,cols,dic){
+  label_data_pt <- function(df, cols, dic) {
+    label_value <- as.character(dic[dic$var_code == cols, "var_pt"])
 
-    label_value = as.character(dic[dic$var_code == cols,'var_pt'])
-
-    df = df %>%
-      dplyr::mutate_at(dplyr::vars(tidyr::matches(cols)),
-                       ~ labelled(.,as.character(dic[dic$var_code == cols,'var_pt']))
+    df <- df %>%
+      dplyr::mutate_at(
+        dplyr::vars(tidyr::matches(cols)),
+        ~ labelled(., as.character(dic[dic$var_code == cols, "var_pt"]))
       )
 
     return(df)
-
   }
 
   ## Load Dictionary
 
-  dic = load_dictionary(param$dataset)
+  dic <- load_dictionary(param$dataset)
 
-  types = as.character(dic$var_code)
-  types = types[types != "0"] ## Remove 0
+  types <- as.character(dic$var_code)
+  types <- types[types != "0"] ## Remove 0
 
 
-  if (language == 'eng'){
+  if (language == "eng") {
 
     # f = dat %>%
     #   dplyr::mutate_at(vars(tidyr::matches(as.character(types[1]))),
     #                    ~ labelled::set_variable_labels(. = as.character(dic[dic$var_code == types[1],'var_eng']))
     #   )
 
-    for (i in 1:length(types)){
-
-      dat = label_data_eng(dat,cols=types[i],dic=dic)
-
+    for (i in 1:length(types)) {
+      dat <- label_data_eng(dat, cols = types[i], dic = dic)
     }
-
   }
 
-  if (language == 'pt'){
-
-    for (i in 1:length(types)){
-
-      dat = label_data_pt(dat,cols=types[i],dic=dic)
-
+  if (language == "pt") {
+    for (i in 1:length(types)) {
+      dat <- label_data_pt(dat, cols = types[i], dic = dic)
     }
   }
 
@@ -324,5 +334,4 @@ load_pam = function(dataset = NULL, raw_data,
   ##########################
 
   return(dat)
-
 }
