1d6bd359c2f4c91b7a54eb01fc66ccdfd7188cb5
IgorRigolon <77173467+IgorRigolon@users.noreply.github.com>
2022-03-09 19:56:45 -0300

    New sysdata

--- data-raw/build_sysdata.R
+++ data-raw/build_sysdata.R
@@ -2,11 +2,9 @@ library(tidyverse)
 
 ## Importing all municipalities
 
-municipalities_2010 <- geobr::lookup_muni("all") %>%
-  as_tibble()
-
-geo_municipalities <- geobr::read_municipality(year = 2020,
-                                               simplified = FALSE)
+municipalities <- geobr::read_municipality(year = 2020,
+                                               simplified = FALSE) %>%
+  sf::st_drop_geometry()
 
 ## Importing Legal Amazon municipalities
 
@@ -19,52 +17,23 @@ utils::download.file(url, destfile = temp, method = "curl")
 
 legal_amazon <- readxl::read_xls(temp, range = "A1:L773")
 
-legal_amazon <- legal_amazon$CD_MUN %>%
+legal_amazon_list <- legal_amazon$CD_MUN %>%
   as.numeric()
 
-## Creating Legal Amazon map data
+## Creating legal amazon dummy
 
-geo_amazon_municipalities <- geo_municipalities %>%
-  filter(code_muni %in% legal_amazon)
-
-## Final municipality datasets
-
-municipalities <- geo_municipalities %>%
-  sf::st_drop_geometry()
-
-amazon_municipalities <- geo_amazon_municipalities %>%
-  sf::st_drop_geometry()
-
-## Aggregated map data by state
-
-geo_states <- geo_municipalities %>%
-  group_by(code_state, abbrev_state, name_state) %>%
-  summarise()
-
-geo_amazon_states <- geo_amazon_municipalities %>%
-  group_by(code_state, abbrev_state, name_state) %>%
-  summarise()
-
-## Aggregated map data
-
-geo_country <- geo_municipalities %>%
-  summarise()
-
-geo_amazon <- geo_amazon_municipalities %>%
-  summarise()
+municipalities <- municipalities %>%
+  mutate(
+    legal_amazon = case_when(
+      code_muni %in% legal_amazon_list ~ 1,
+      TRUE ~ 0
+    )
+  )
 
 ## Adding to sysdata
 
 usethis::use_data(
-  municipalities_2010,
   municipalities,
-  geo_municipalities,
-  geo_states,
-  geo_country,
-  amazon_municipalities,
-  geo_amazon_municipalities,
-  geo_amazon_states,
-  geo_amazon,
   internal = TRUE,
   overwrite = TRUE
 )
