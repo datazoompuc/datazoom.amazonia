45729fb3daebeb5b171a557b079900ce482d440d
Luiz Guilherme Lopes Moussatche <luizguilherme.lopesm@gmail.com>
2022-06-14 13:32:34 -0300

    creating imazon function
    creating imazon function that will scrape data from datazoom google drive

---
+++ R/imazon.R
@@ -0,0 +1,32 @@
+library(sf)
+library(tidyverse)
+library(googledrive)
+
+imazon_sf= read_sf("C:\\Users\\lugui\\Desktop\\DataZoom\\IMAZON\\Fronteiras_Amazonia\\Fronteiras_Amazonia_2020.shp")
+
+
+load_imazon= function(dataset = NULL, raw_data,
+                      geo_level, language = "eng"){
+
+
+# Checking for googledrive package (in Suggests)
+
+  if (!requireNamespace("googledrive", quietly = TRUE)) {
+    stop(
+      "Package \"googledrive\" must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+
+
+  if (param$geo_level == "municipality") {
+  message("Please follow the steps from `googledrive` package to download the data. This may take a while.")
+  }
+
+dat <- external_download(
+  dataset = param$dataset,
+  source = "imazon",
+  geo_level = param$geo_level
+)
+
+}
