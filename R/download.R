ed813ce281753a028c126aac75fffa7f3f2ace9f
Francisco Cavalcanti <francisco.lima.cavalcanti@gmail.com>
2022-06-28 19:11:47 -0300

    Insert download source for IMAZON shapefiles

--- R/download.R
+++ R/download.R
@@ -511,6 +511,16 @@ external_download <- function(dataset = NULL, source = NULL, year = NULL,
     }
   }
 
+  ############
+  ## IMAZON ##
+  ############
+
+  if (source == "imazon_shp") {
+    if (geo_level == "municipality") {
+      path <- "https://docs.google.com/uc?export=download&id=1JHc2J_U8VXHVuWVsi8wVBnNzZ37y1ehv"
+    }
+  }
+
   #####################
   ## GeoBR Shapefile ##
   #####################
@@ -557,6 +567,9 @@ external_download <- function(dataset = NULL, source = NULL, year = NULL,
       file_extension <- ".dbc"
     }
   }
+  if (source == "imazon_shp") {
+    file_extension <- ".rds"
+  }
 
   # !!!  We should Change This to a Curl Process
 
@@ -584,6 +597,13 @@ external_download <- function(dataset = NULL, source = NULL, year = NULL,
       googledrive::drive_download(path, path = temp, overwrite = TRUE)
     }
   }
+
+  if (source == "imazon_shp") {
+    if (geo_level == "municipality") {
+      googledrive::drive_download(path, path = temp, overwrite = TRUE)
+    }
+  }
+
   if (source == "terraclimate") {
     utils::download.file(url = path, destfile = temp, method = "curl")
   }
@@ -1083,8 +1103,13 @@ datasets_link <- function() {
 
     ## Shapefile from github repository
 
-    "Internal", "geo_municipalities", NA, "2020", "Municipality", "https://raw.github.com/datazoompuc/datazoom.amazonia/master/data-raw/geo_municipalities.rds"
-  )
+    "Internal", "geo_municipalities", NA, "2020", "Municipality", "https://raw.github.com/datazoompuc/datazoom.amazonia/master/data-raw/geo_municipalities.rds",
+
+    ############
+    ## IMAZON ##
+    ############
+    "Imazon", "imazon_shp", NA, "2020", "Municipality", "https://drive.google.com/drive/u/1/folders/1EAOABo1GVKT3YsYkhtgJI9ckB3RULJSC"
+     )
 
   return(link)
 }
