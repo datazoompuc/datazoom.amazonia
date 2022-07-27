81424774bdc3839258e8b5ee8d10636ede75fef8
Igor Rigolon <igor.rilave@hotmail.com>
2022-07-26 23:04:11 -0300

    unify ibama functions
    incorporating ibama_multas as new datasets in the load_ibama function
    
    allowing download of many states at a time
    
    adding ibge municipality codes and doing some more data cleaning to ibama multas

--- R/ibama.R
+++ R/ibama.R
@@ -2,9 +2,9 @@
 #'
 #' @description Loads information on environmental fines at the municipality or state levels considering the Amazon region
 #'
-#' @param dataset A dataset name ("areas_embargadas")
+#' @param dataset A dataset name ("areas_embargadas", "distributed_fines", "collected_fines")
 #' @param raw_data A \code{boolean} setting the return of raw or processed data
-#' @param geo_level A \code{string} that defines the geographic level of the data. Can be only "municipality".
+#' @param states A \code{string} specifying for which states to download the data. It is "all" by default, but can be a single state such as "AC" or any vector such as c("AC", "AM"). Does not apply to the "areas_embargadas" dataset.
 #' @param language A \code{string} that indicates in which language the data will be returned. Currently, only Portuguese ("pt") and English ("eng") are supported.
 #' @param legal_amazon_only A \code{boolean} setting the return of Legal Amazon Data or not
 #'
@@ -24,11 +24,15 @@
 #' @export
 
 
-load_ibama <- function(dataset = "areas_embargadas",
+load_ibama <- function(dataset,
                        raw_data,
-                       geo_level = "municipality",
-                       language = "eng",
-                       legal_amazon_only = FALSE) {
+                       states = "all",
+                       language = "eng") {
+
+  ##############################
+  ## Binding Global Variables ##
+  ##############################
+
   survey <- link <- legal_amazon <- codigo_ibge_municipio_embargo <- NULL
   municipio_embargo <- uf_embargo <- julgamento <- infracao <- data_de_insercao_na_lista <- NULL
   cpf_ou_cnpj <- cod_municipio <- ano <- mes <- NULL
@@ -43,9 +47,15 @@ load_ibama <- function(dataset = "areas_embargadas",
 
   param <- list()
   param$dataset <- dataset
-  param$geo_level <- geo_level
   param$language <- language
   param$raw_data <- raw_data
+
+  if (states == "all"){
+    param$states <- c("RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO", "DF")
+  }
+  if (dataset == "areas_embargadas"){
+    param$states <- "all"
+  }
 
   param$survey_name <- datasets_link() %>%
     dplyr::filter(dataset == param$dataset) %>%
@@ -71,27 +81,133 @@ load_ibama <- function(dataset = "areas_embargadas",
   ## Download ##
   ##############
 
-  dat <- external_download(
-    dataset = param$dataset,
-    source = "ibama",
-    geo_level = param$geo_level
-  )
+  # Downloads each state separately
 
+  dat <- param$states %>%
+    purrr::imap(
+      function(uf, iteration) {
+        base::message(paste0("Downloading ", uf, " (", iteration, " out of ", length(param$states), ")"))
 
-  ## Filter for Legal Amazon
-  if (legal_amazon_only) {
-    legal_amazon_filtered <- municipalities %>% dplyr::filter(legal_amazon == 1)
+        external_download(
+          source = "ibama",
+          dataset = param$dataset,
+          state = uf
+        )
+      }
+    )
 
-    dat <- dat %>%
-      dplyr::filter(municipio_infracao %in% unique(legal_amazon_filtered$name_muni))
-  }
-
+  names(dat) <- param$states
 
   ## Return Raw Data
-  if (raw_data == TRUE) {
+
+  if (param$raw_data) {
     return(dat)
   }
 
+  ######################
+  ## Data Engineering ##
+  ######################
+
+  dat <- dat %>%
+    dplyr::bind_rows() %>%
+    janitor::clean_names()
+
+  if (dataset %in% c("collected_fines", "distributed_fines")){
+
+    # data come without municipality codes, only their names. +so we turn everything lowercase and remove accents to try and match.
+
+    dat <- dat %>%
+      dplyr::mutate( # removing accents from municipality names
+      dplyr::across(municipio, ~ stringi::stri_trans_general(., id = "Latin-ASCII"))
+    ) %>%
+      dplyr::mutate(dplyr::across(municipio, tolower)) # making all municipality names lowercase
+
+    geo <- municipalities %>%
+      dplyr::select(
+        code_muni,
+        name_muni,
+        "uf" = abbrev_state,
+        legal_amazon
+      )
+
+    # Removing accents from the dataset with IBGE codes to make the city names compatible and merge
+
+    geo <- geo %>%
+      dplyr::mutate(
+        dplyr::across(
+          name_muni,
+          ~ stringi::stri_trans_general(., id = "Latin-ASCII"),
+          .names = "municipio"
+        )
+      ) %>%
+      dplyr::mutate(dplyr::across(municipio, tolower))
+
+    # 41 municipalities don't match due to spelling inconsistencies
+
+    dat <- dat %>%
+      dplyr::mutate(
+        municipio = dplyr::case_when(
+          municipio == "sao luiz do anuaa" & uf == "RR" ~ "sao luiz",
+          municipio == "eldorado dos carajas" & uf == "PA" ~ "eldorado do carajas",
+          municipio == "santa isabel do para" & uf == "PA" ~ "santa izabel do para",
+          municipio == "couto de magalhaes" & uf == "TO" ~ "couto magalhaes",
+          municipio == "fortaleza do tabocao" & uf == "TO" ~ "tabocao",
+          municipio == "pau d arco" & uf == "TO" ~ "pau d'arco",
+          municipio == "sao valerio da natividade" & uf == "TO" ~ "sao valerio",
+          municipio == "governador edson lobao" & uf == "MA" ~ "governador edison lobao",
+          municipio == "assu" & uf == "RN" ~ "acu",
+          municipio == "presidente juscelino" & uf == "RN" ~ "serra caiada",
+          municipio == "olho-d'agua do borges" & uf == "RN" ~ "olho d'agua do borges",
+          municipio == "augusto severo" & uf == "RN" ~ "campo grande",
+          municipio == "campo de santana" & uf == "PB" ~ "tacima",
+          municipio == "santarem" & uf == "PB" ~ "joca claudino",
+          municipio == "sao domingos de pombal" & uf == "PB" ~ "sao domingos",
+          municipio == "lagoa do itaenga" & uf == "PE" ~ "lagoa de itaenga",
+          municipio == "iguaraci" & uf == "PE" ~ "iguaracy",
+          municipio == "belem de sao francisco" & uf == "PE" ~ "belem do sao francisco",
+          municipio == "amparo de sao francisco" & uf == "SE" ~ "amparo do sao francisco",
+          municipio == "santa teresinha" & uf == "BA" ~ "santa terezinha",
+          municipio == "muquem de sao francisco" & uf == "BA" ~ "muquem do sao francisco",
+          municipio == "brasopolis" & uf == "MG" ~ "brazopolis",
+          municipio == "passa-vinte" & uf == "MG" ~ "passa vinte",
+          municipio == "sao thome das letras" & uf == "MG" ~ "sao tome das letras",
+          municipio == "dona eusebia" & uf == "MG" ~ "dona euzebia",
+          municipio == "justinopolis" & uf == "MG" ~ "ribeirao das neves",
+          municipio == "trajano de morais" & uf == "RJ" ~ "trajano de moraes",
+          municipio == "embu" & uf == "SP" ~ "embu das artes",
+          municipio == "moji mirim" & uf == "SP" ~ "mogi mirim",
+          municipio == "florinia" & uf == "SP" ~ "florinea",
+          municipio == "biritiba-mirim" & uf == "SP" ~ "biritiba mirim",
+          municipio == "sao luis do paraitinga" & uf == "SP" ~ "sao luiz do paraitinga",
+          municipio == "grao para" & uf == "SC" ~ "grao-para",
+          municipio == "presidente castelo branco" & uf == "SC" ~ "presidente castello branco",
+          municipio == "passos de torres" & uf == "SC" ~ "passo de torres",
+          municipio == "santana do livramento" & uf == "RS" ~ "sant'ana do livramento",
+          municipio == "poxoreo" & uf == "MT" ~ "poxoreu",
+          municipio == "colinas de goiais" & uf == "GO" ~ "colinas do sul",
+          TRUE ~ municipio
+        )
+      )
+
+    dat <- dat %>%
+      dplyr::mutate(uf = dplyr::case_when(
+        municipio == "ponte alta do norte" & uf == "GO" ~ "SC",
+        TRUE ~ uf
+      )
+    )
+
+    # Changing dates to date format
+
+    dat <- dat %>%
+      dplyr::mutate(
+        dplyr::across(dplyr::any_of(c("data_auto", "data_pagamento")), as.Date, format = "%d/%m/%Y")
+      ) %>%
+      dplyr::mutate(
+        dplyr::across(ultima_atualizacao_relatorio, as.POSIXct, format = "%d/%m/%Y %H:%M")
+      )
+  }
+
+  if (dataset == "areas_embargadas"){
 
   ## Aggregate to municipality-level
   dat <- dat %>%
@@ -118,6 +234,16 @@ load_ibama <- function(dataset = "areas_embargadas",
       .groups = "drop"
     )
 
+  geo <- municipalities %>%
+    dplyr::select(
+      code_muni,
+      "municipio" = name_muni,
+      "uf" = abbrev_state,
+      legal_amazon
+    )
+
+  }
+
   ## Adding IBGE municipality codes
 
   municipalities <- municipalities %>%
@@ -126,11 +252,16 @@ load_ibama <- function(dataset = "areas_embargadas",
       cod_municipio = code_muni
     )
 
+  # Merging with IBGE municipalities
+
   dat <- dat %>%
-    dplyr::left_join(municipalities, by = "municipio")
+    dplyr::left_join(geo, by = c("municipio", "uf"))
 
-  ## Final variables and translation
+  ################################
+  ## Harmonizing Variable Names ##
+  ################################
 
+  if (dataset == "areas_embargadas"){
   if (param$language == "pt") {
     dat_mod <- dat %>%
       dplyr::select(
@@ -150,6 +281,50 @@ load_ibama <- function(dataset = "areas_embargadas",
         n_unique_cpf_cnpj = n_cpf_cnpj_unicos
       ) %>%
       dplyr::arrange(year, month, municipality)
+  }
+  }
+
+  if (dataset %in% c("collected_fines", "distributed_fines")){
+
+    dat <- dat %>%
+      dplyr::select(-municipio) %>%
+      dplyr::relocate(code_muni, name_muni, uf, data_auto)
+
+    if (param$language == "pt") {
+      dat_mod <- dat %>%
+        dplyr::rename(
+          "cod_municipio" = name_muni,
+          "municipio" = name_muni,
+          "penalidade" = tipo_auto,
+          "data_penalidade" = data_auto,
+          "valor_penalidade" = valor_do_auto,
+          "amazonia_legal" = legal_amazon
+        )
+    }
+
+    if (param$language == "eng") {
+        dat_mod <- dat %>%
+          dplyr::rename_with(dplyr::recode,
+            code_muni = "municipality_code",
+            name_muni = "municipality",
+            uf = "state",
+            data_auto = "penalty_date",
+            no_ai = "report_number",
+            nome_ou_razao_social = "name_or_corporate_name",
+            status_debito = "debt_status",
+            tipo_auto = "penalty",
+            tipo_infracao = "infraction",
+            enquadramento_legal = "legal_framework",
+            valor_do_auto = "penalty_value",
+            moeda = "currency",
+            parcela = "installment",
+            quantidade_de_parcelas = "number_of_installments",
+            valor_base_da_parcela = "base_installment_value",
+            valor_pago = "value_paid",
+            data_pagamento = "payment_date",
+            ultima_atualizacao_relatorio = "report_last_update"
+          )
+    }
   }
 
   ####################
