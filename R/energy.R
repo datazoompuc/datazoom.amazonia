6f80703d2ffd9c65b0e4e66606a7a701786791ee
Victor Hugo Schieck Terziani <vhsterziani@gmail.com>
2022-11-07 14:48:01 -0300

    Update energy.R

--- R/energy.R
+++ R/energy.R
@@ -84,19 +84,113 @@ load_energy <- function(dataset = "SIGA", raw_data = FALSE, time_period,
 
         #Select sheets with 'UF' in the name
 
-        sheets_selected <- all_sheets %>% filter(str_detect(all_sheets[,1], "UF") == T)
+        sheets_selected <- as.data.frame(all_sheets) %>% filter(str_detect(all_sheets[,1], "UF") == T)
 
         #Function to tidy a single sheet
         manipular <- function(sheet_name){
+
+          #df sera manipulado
+          df <- as.data.frame(dat[sheet_name])
+
+          #transformar nomes das colunas nos meses referentes (para usar pivot_longer)
+          df <- df %>% janitor::row_to_names(5) %>% janitor::clean_names()
+
+          #fazer o pivot longer
+          df2 <- pivot_longer(df, cols = c(2:length(df)))
+
+          #renomear colunas
+          df2[1,4] <- NA
+          names(df2) <- c("Estado", "Mes", "Valor", "Ano")
+
+          #criar uma coluna de ano onde entra o ano de cada observacao de acordo com o numero associado ao mes
+          for(i in 1:nrow(df2)){
+            if(str_detect(df2[i,"Mes"], "2") == TRUE){
+              df2[i,4] <- 2005
+            } else if(str_detect(df2[i,"Mes"], "3") == TRUE){
+              df2[i,4] <- 2006
+            } else if(str_detect(df2[i,"Mes"], "4") == TRUE){
+              df2[i,4] <- 2007
+            } else if(str_detect(df2[i,"Mes"], "5") == TRUE){
+              df2[i,4] <- 2008
+            } else if(str_detect(df2[i,"Mes"], "6") == TRUE){
+              df2[i,4] <- 2009
+            } else if(str_detect(df2[i,"Mes"], "7") == TRUE){
+              df2[i,4] <- 2010
+            } else if(str_detect(df2[i,"Mes"], "8") == TRUE){
+              df2[i,4] <- 2011
+            } else if(str_detect(df2[i,"Mes"], "9") == TRUE){
+              df2[i,4] <- 2012
+            } else if(str_detect(df2[i,"Mes"], "10") == TRUE){
+              df2[i,4] <- 2013
+            } else if(str_detect(df2[i,"Mes"], "11") == TRUE){
+              df2[i,4] <- 2014
+            } else if(str_detect(df2[i,"Mes"], "12") == TRUE){
+              df2[i,4] <- 2015
+            } else if(str_detect(df2[i,"Mes"], "13") == TRUE){
+              df2[i,4] <- 2016
+            } else if(str_detect(df2[i,"Mes"], "14") == TRUE){
+              df2[i,4] <- 2017
+            } else if(str_detect(df2[i,"Mes"], "15") == TRUE){
+              df2[i,4] <- 2018
+            } else if(str_detect(df2[i,"Mes"], "16") == TRUE){
+              df2[i,4] <- 2019
+            } else if(str_detect(df2[i,"Mes"], "17") == TRUE){
+              df2[i,4] <- 2020
+            } else if(str_detect(df2[i,"Mes"], "18") == TRUE){
+              df2[i,4] <- 2021
+            } else if(str_detect(df2[i,"Mes"], "19") == TRUE){
+              df2[i,4] <- 2022
+            } else if(str_detect(df2[i,"Mes"], "20") == TRUE){
+              df2[i,4] <- 2023
+            } else if(str_detect(df2[i,"Mes"], "21") == TRUE){
+              df2[i,4] <- 2024
+            } else {
+              df2[i,4] <- 2004
+            }
+          }
+
+          #Fazer com que as observacoes dos meses nao tenham numeros
+          for(i in 1:nrow(df2)){
+            if(str_detect(df2[i,"Mes"], "jan") == TRUE){
+              df2[i,"Mes"] <- "JAN"
+            } else if(str_detect(df2[i,"Mes"], "fev") == TRUE){
+              df2[i,"Mes"] <- "FEV"
+            } else if(str_detect(df2[i,"Mes"], "mar") == TRUE){
+              df2[i,"Mes"] <- "MAR"
+            } else if(str_detect(df2[i,"Mes"], "abr") == TRUE){
+              df2[i,"Mes"] <- "ABR"
+            } else if(str_detect(df2[i,"Mes"], "mai") == TRUE){
+              df2[i,"Mes"] <- "MAI"
+            } else if(str_detect(df2[i,"Mes"], "jun") == TRUE){
+              df2[i,"Mes"] <- "JUN"
+            } else if(str_detect(df2[i,"Mes"], "jul") == TRUE){
+              df2[i,"Mes"] <- "JUL"
+            } else if(str_detect(df2[i,"Mes"], "ago") == TRUE){
+              df2[i,"Mes"] <- "AGO"
+            } else if(str_detect(df2[i,"Mes"], "set") == TRUE){
+              df2[i,"Mes"] <- "SET"
+            } else if(str_detect(df2[i,"Mes"], "out") == TRUE){
+              df2[i,"Mes"] <- "OUT"
+            } else if(str_detect(df2[i,"Mes"], "nov") == TRUE){
+              df2[i,"Mes"] <- "NOV"
+            } else if(str_detect(df2[i,"Mes"], "dez") == TRUE){
+              df2[i,"Mes"] <- "DEZ"
+            }
+          }
+
+          return(df2)
 
         }
 
         #Run function that tidies sheets and bind all sheets together
 
-        final_dat <- data.frame()
+        final_dat <- data.frame(Estado = as.character(NULL),
+                                Mes = as.character(NULL),
+                                Ano = as.integer(NULL))[numeric(0), ]
+
         for(i in 1:length(sheets_selected)){
-          single_sheet <- manipular(dat[i])
-          final_dat <- rbind(final_dat, single_sheet)
+          single_sheet <- manipular(paste0(sheets_selected[i,1]))
+          final_dat <- full_join(final_dat, single_sheet, by = c("Estado", "Mes", "Ano"))
         }
 
         #If user selects region level
