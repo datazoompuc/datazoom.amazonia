3ca8ceeb0290dd1c61ad6219a0226d3314835f93
Igor Rigolon <igor.rilave@hotmail.com>
2025-02-27 19:43:35 -0300

    adding a helpfile for the entire package

---
+++ R/datazoom.amazonia-package.R
@@ -0,0 +1,12 @@
+#' @description Functions to download and treat data regarding the Brazilian Amazon region from a variety of official sources.
+#' @useDynLib datazoom.amazonia, .registration = TRUE
+#'
+#' @keywords internal
+#' @encoding UTF-8
+#' @importFrom magrittr %>%
+
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
