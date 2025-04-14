03afc2c5596b9a4b81d0c46ab545dbe9979f3539
João Paulo Barbosa <joao.31582129@gmail.com>
2025-04-14 20:01:20 -0300

    update datasus_sih rascunho

--- R/datasus_sih_rascunho.R
+++ R/datasus_sih_rascunho.R
@@ -4,32 +4,15 @@ library(devtools)
 devtools::install_github("datazoompuc/datazoom.amazonia")
 devtools::load_all()
 
-# Definir a URL do arquivo e o caminho de destino
-url <- "ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RDAC2302.dbc"
-destino <- "IT_SIHSUS_1603.pdf"  # Nome do arquivo no seu computador
-
-# Baixar o arquivo
-download.file(url, destfile = destino, mode = "wb")
-
-url <- "ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RDDF2302.dbc"
-destino <- "RDDF2302.dbc"
-
-# Baixar o arquivo .dbc do SIHSUS
-download.file(url, destfile = destino, mode = "wb")
-
-# Verifica se baixou e lê o conteúdo usando o pacote certo
-if (file.exists(destino)) {
-  message("Download concluído com sucesso. Lendo o arquivo com datazoom.amazonia...")
-
-  dados <- datazoom.amazonia:::read.dbc(destino)
-  View(dados)
-} else {
-  message("Falha no download.")
+baixar_dbc <- function(url, destino) {
+  download.file(url, destfile = destino, mode = "wb")
+  if (file.exists(destino)) {
+    message(paste("Download concluído:", destino))
+    return(datazoom.amazonia:::read.dbc(destino))
+  } else {
+    stop("Falha no download de ", destino)
+  }
 }
 
-datazoom.amazonia:::read.dbc()
-
-
-#
-
-data <- load_datasus("datasus_sih", 2010, states = "AC", raw_data = TRUE )
+dados1 <- baixar_dbc("ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RDAC2302.dbc", "RDAC2302.dbc")
+dados2 <- baixar_dbc("ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Dados/RDDF2302.dbc", "RDDF2302.dbc")
