7ff3d05a6058274065d33394b02248e6cec110b8
João Paulo Barbosa <joao.31582129@gmail.com>
2025-05-11 16:10:01 -0300

    Summary

--- datasus_sih_testes.R
+++ datasus_sih_testes.R
@@ -29,7 +29,7 @@ sinasc_eng <- load_datasus(
 datasus_sih_rd <- load_datasus(
   dataset = "datasus_sih_rd",
   time_period = 2023,
-  raw_data = TRUE,
+  raw_data = FALSE,
   language = "pt",
   states = "AC"
   )
