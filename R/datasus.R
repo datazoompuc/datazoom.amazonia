3d8e56ba7973833a72df85fbb4e6dbe32f627240
Bernardo Furlanetto Sieira <bernardo.sieira@gmail.com>
2025-08-19 23:29:26 -0300

    devtools::check() solution

--- R/datasus.R
+++ R/datasus.R
@@ -12,11 +12,14 @@
 #' @return \code{NULL}. Always returns empty.
 #'
 #' @export
-
 load_datasus <- function(dataset = NULL, raw_data = NULL, time_period = NULL,
                          language = "eng") {
-  ifelse(language == "pt",
-         warning("A função `load_datasus` não faz mais parte do pacote e não está mais disponível. Novidades em breve.", call. = FALSE),
-         warning("The `load_datasus` function is no longer part of the package and is no longer available. Updates on the matter coming soon.", call. = FALSE))
+  if (language == "pt") {
+    warning("A fun\u00e7\u00e3o `load_datasus` n\u00e3o faz mais parte do pacote e n\u00e3o est\u00e1 mais dispon\u00edvel. Novidades em breve.",
+            call. = FALSE)
+  } else {
+    warning("The `load_datasus` function is no longer part of the package and is no longer available. Updates on the matter coming soon.",
+            call. = FALSE)
+  }
   return(invisible(NULL))
 }
