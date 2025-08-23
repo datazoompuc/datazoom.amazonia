edd75332792c53c95d512ae782077002ff8556c6
Bernardo Furlanetto Sieira <bernardo.sieira@gmail.com>
2025-08-22 22:22:04 -0300

    Atualização da mensagem
    Coloquei saúde sem acento no em inglês!!!

--- R/datasus.R
+++ R/datasus.R
@@ -15,10 +15,10 @@
 load_datasus <- function(dataset = NULL, raw_data = NULL, time_period = NULL,
                          language = "eng") {
   if (language == "pt") {
-    warning("A fun\u00e7\u00e3o `load_datasus` n\u00e3o faz mais parte do pacote e n\u00e3o est\u00e1 mais dispon\u00vel. Instale o novo pacote datazoom.saude para carregar esses dados.",
+    warning("A fun\u00e7\u00e3o `load_datasus` n\u00e3o faz mais parte do pacote e n\u00e3o est\u00e1 mais dispon\u00vel. As funcionalidades ser\u00E3o integradas no novo pacote chamado Datazoom Sa\u00FAde, atualmente em desenvolvimento.",
             call. = FALSE)
   } else {
-    warning("The `load_datasus` function is no longer part of the package and is no longer available. Updates on the matter coming soon. To load this data, install the new datazoom.saude package.",
+    warning("The `load_datasus` function is no longer part of the package and is no longer available. The functionalities will be explored in a new package called Datazoom Saude, currently under development.",
             call. = FALSE)
   }
   return(invisible(NULL))
