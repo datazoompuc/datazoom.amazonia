192ff84d4ff833cf48fd55ebe062431bbc17d2b0
Bernardo Furlanetto Sieira <bernardo.sieira@gmail.com>
2026-05-18 01:04:23 -0300

    energy_state_panel

--- R/epe.R
+++ R/epe.R
@@ -2,7 +2,7 @@
 #'
 #' @description Electrical Energy Monthly Consumption per Class or Industrial Sector
 #'
-#' @param dataset Dataset name: "consumer_energy_consumption", "industrial_energy_consumption", or "national_energy_balance"
+#' @param dataset Dataset name: "consumer_energy_consumption", "industrial_energy_consumption", "national_energy_balance", or "energy_state_panel"
 #' @param geo_level Geographical level: "state" or "subsystem". Only applies to consumer or industrial datasets.
 #' @inheritParams load_baci
 #'
@@ -32,7 +32,7 @@ load_epe <- function(dataset, geo_level = "state", raw_data = FALSE, language = 
 
   uf <- regiao <- sistema <- classe <- tipo_consumidor <- consumo <- consumidores <- setor_industrial <- data_excel <- NULL
 
-  conta <- bloco <- fonte <- valor <- ano <- account <- year <- value <- data <- NULL
+  conta <- bloco <- fonte <- valor <- ano <- account <- year <- value <- data <- total_produzido <- hidro <- eolica <- solar <- nuclear <- termo <- cana <- lenha <- lixivia <- outras_fontes_renovaveis <- carvao_vapor <- gas_natural <- gas_de_coqueira <- combustivel <- diesel <- outras_fontes_nao_renovaveis <- amz_legal <- state <- legal_amazon <- total_produced <- hydro <- wind <- other_renewable_sources <- steam_coal <- natural_gas <- coke_oven_gas <- fuel_oil <- diesel_oil <- other_non_renewable_sources <- NULL
 
   #############################
   ## Define Basic Parameters ##
@@ -74,6 +74,9 @@ load_epe <- function(dataset, geo_level = "state", raw_data = FALSE, language = 
     }
   }
 
+  if (param$dataset == "energy_state_panel") {
+    sheets <- "8.1 part 3"
+  }
   dat <- external_download(
     source = param$source,
     dataset = param$dataset,
@@ -159,6 +162,58 @@ load_epe <- function(dataset, geo_level = "state", raw_data = FALSE, language = 
       dplyr::select(-dplyr::any_of(c("data", "data_versao")))
   }
 
+
+  if (param$dataset == "energy_state_panel") {
+    raw <- dat[[1]]
+
+    amz_legal_estados <- c("Amapa", "Acre", "Amazonas", "Mato Grosso",
+                           "Tocantins", "Maranhao", "Rondonia", "Roraima", "Para")
+
+    # Cada bloco de ano começa com "ANO BASE XXXX" na col 1
+    ano_rows <- which(grepl("ANO BASE", as.character(raw[[1]]), ignore.case = TRUE))
+    anos     <- as.integer(stringr::str_extract(as.character(raw[[1]][ano_rows]), "\\d{4}"))
+    raw$ano_tag <- NA_integer_
+    for (i in seq_along(ano_rows)) {
+      end_row <- if (i < length(ano_rows)) ano_rows[i + 1] - 1 else nrow(raw)
+      raw$ano_tag[ano_rows[i]:end_row] <- anos[i]
+    }
+
+    dat_mod <- raw %>%
+      dplyr::filter(!is.na(ano_tag)) %>%
+      dplyr::filter(!is.na(suppressWarnings(as.numeric(as.character(.[[2]]))))) %>%
+      dplyr::filter(stringr::str_detect(as.character(.[[1]]), "[:lower:]")) %>%
+      dplyr::select(
+        uf                             = 1,
+        total_produzido                = 2,
+        hidro                          = 3,
+        eolica                         = 4,
+        solar                          = 5,
+        nuclear                        = 6,
+        termo                          = 7,
+        cana                           = 8,
+        lenha                          = 9,
+        lixivia                        = 10,
+        outras_fontes_renovaveis       = 11,
+        carvao_vapor                   = 12,
+        gas_natural                    = 13,
+        gas_de_coqueira                = 14,
+        combustivel                    = 15,
+        diesel                         = 16,
+        outras_fontes_nao_renovaveis   = 17,
+        ano                            = ano_tag
+      ) %>%
+      dplyr::mutate(
+        uf = dplyr::case_when(
+          uf == "Mato G. do Sul"  ~ "Mato Grosso do Sul",
+          uf == "Rio G. do Sul"   ~ "Rio Grande do Sul",
+          uf == "Rio G. do Norte" ~ "Rio Grande do Norte",
+          TRUE ~ uf
+        ),
+        uf = stringi::stri_trans_general(uf, "Latin-ASCII"),
+        amz_legal = dplyr::if_else(uf %in% amz_legal_estados, 1L, 0L)
+      ) %>%
+      dplyr::mutate(dplyr::across(-c(uf, amz_legal), as.numeric))
+  }
   ################################
   ## Harmonizing Variable Names ##
   ################################
@@ -210,6 +265,32 @@ load_epe <- function(dataset, geo_level = "state", raw_data = FALSE, language = 
           industrial_sector = setor_industrial,
           consumption = consumo,
           date = data
+        )
+    }
+  }
+
+  if (param$dataset == "energy_state_panel") {
+    if (param$language == "eng") {
+      dat_mod <- dat_mod %>%
+        dplyr::rename(
+          state                        = uf,
+          legal_amazon                 = amz_legal,
+          year                         = ano,
+          total_produced               = total_produzido,
+          hydro                        = hidro,
+          wind                         = eolica,
+          nuclear                      = nuclear,
+          thermal                      = termo,
+          sugar_cane                   = cana,
+          firewood                     = lenha,
+          black_liquor                 = lixivia,
+          other_renewable_sources      = outras_fontes_renovaveis,
+          steam_coal                   = carvao_vapor,
+          natural_gas                  = gas_natural,
+          coke_oven_gas                = gas_de_coqueira,
+          fuel_oil                     = combustivel,
+          diesel_oil                   = diesel,
+          other_non_renewable_sources  = outras_fontes_nao_renovaveis
         )
     }
   }
